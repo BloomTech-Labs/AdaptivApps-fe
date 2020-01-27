24915d3a479dea5b97faba13e86116f68b8a7a63
Maddy <mmcintosh2k14@gmail.com>
2020-01-27 12:41:02 -0500

    creates GET request to server and displays response message

--- src/home.js
+++ src/home.js
@@ -1,17 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import chif from './photos/chif.chif';
 
 
 
 const Home = () => {
+  // Set message received from server.
+  const [response, setResponse] = useState("");
+
     useEffect(() => {
         window.chifPlayer.streamFiles();
+
+        // GET Request to AdaptivApps API to retrieve data and setResponse with a message
+        axios.get('https://angel-city-sports.herokuapp.com/')
+        .then(res =>  {
+          const message = res.data.api;
+          console.log(message);
+          setResponse(message);
+        })
+        .catch(err => {
+          console.log('Error retrieving data from API.', err);
+        });
+        
     }, []);
     return (
         <div className= "chifContain">
-            <chear className="chifOne" src={require('./photos/chif.chif')}></chear>
-            <chear className="chifOne" src={require('./photos/chif2.chif')}></chear>
-            <chear className="chifOne" src={require('./photos/chif3.chif')}></chear>
+          <h1>{response}</h1>
+          <chear className="chifOne" src={chif}></chear>
+          <chear className="chifOne" src={require('./photos/chif2.chif')}></chear>
+          <chear className="chifOne" src={require('./photos/chif3.chif')}></chear>
         </div>
     )
 }
