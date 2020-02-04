381b324b057a94576f7e1047defb9ad2befd3b90
Maddy <mmcintosh2k14@gmail.com>
2020-02-04 17:12:02 -0500

    installs auth0 dependencies creates history.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AccessibilityModal from './components/AccessibilityModal';
 import Header from './components/Header';
 import Home from './components/LandingPage';
-import Signup from './components/users/SignUp';
+// import Signup from './components/users/SignUp';
 import UserDashboard from './components/users/UserDashboard';
 import AdminDashboard from './components/admin/AdminDashboard';
 import PrivateRoute from './utils/PrivateRoute';
@@ -11,34 +11,25 @@ import Logout from './components/Logout';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import EditProfile from './components/ProfileForm';
 
-// Okta auth imports
-// import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 
 import './App.css';
 
 function App() {
 	return (
     <div className='App'>
-      {/* <Security 
-        issuer=""
-        clientId=""
-        redirectUri={window.location.origin + "/implicit/callback"}
-        onAuthRequired={onAuthRequired}
-      > */}
-				<Header />
-				<AccessibilityModal />
-				<Route exact path='/Edit' component={EditProfile} />
-				<Route exact path='/' component={Home} />
-				<Route exact path='/Signup' component={Signup} />
-				<Route exact path='/SignIn' component={Login} />
-				<Route exact path='/logout' component={Logout} />
-				<PrivateRoute
-					exact
-					path='/dashboard/admin'
-					component={AdminDashboard}
-				/>
-				<PrivateRoute exact path='/dashboard' component={UserDashboard} />
-		  {/* </Security> */}
+      <Header />
+      <AccessibilityModal />
+      <Route exact path='/Edit' component={EditProfile} />
+      <Route exact path='/' component={Home} />
+      {/* <Route exact path='/Signup' component={Signup} /> */}
+      <Route exact path='/SignIn' component={Login} />
+      <Route exact path='/logout' component={Logout} />
+      <PrivateRoute
+        exact
+        path='/dashboard/admin'
+        component={AdminDashboard}
+      />
+      <PrivateRoute exact path='/dashboard' component={UserDashboard} />
     </div>
 	);
 }
--- src/components/users/SignUp.js
+++ src/components/users/SignUp.js
@@ -1,131 +1,131 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import { signUp } from "../../ReduxStuff/actions/actions";
-import styled from 'styled-components'
+// import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// import { connect } from "react-redux";
+// import { signUp } from "../../ReduxStuff/actions/actions";
+// import styled from 'styled-components'
 
-const CreateAccount = props => {
-    const [background, setBackground] = useState(props.backGround)
+// const CreateAccount = props => {
+//     const [background, setBackground] = useState(props.backGround)
 
-    useEffect(() => {
-        if (props.backGround == "lightblue") {
-            setBackground("white")
-        }
-        else {
-            setBackground(props.backGround)
-        }
-    }, [props]);
+//     useEffect(() => {
+//         if (props.backGround == "lightblue") {
+//             setBackground("white")
+//         }
+//         else {
+//             setBackground(props.backGround)
+//         }
+//     }, [props]);
 
-    console.log(props)
-    const [form, setForm] = useState({
-        full_name: "",
-        location: "",
-        email: "",
-        username: "",
-        password: ""
-    });
+//     console.log(props)
+//     const [form, setForm] = useState({
+//         full_name: "",
+//         location: "",
+//         email: "",
+//         username: "",
+//         password: ""
+//     });
 
-    const Label = styled.label`
-    font-size: ${props.fontSize - 10}px;
-    color: ${props.fontColor};
-    `
-    const Header = styled.h1`
-    font-size: ${props.fontSize}px;
-    color: ${props.fontColor};
-    `
-    const Form = styled.form`
-    background-color: ${background};
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding-bottom: 5%;
-    `
+//     const Label = styled.label`
+//     font-size: ${props.fontSize - 10}px;
+//     color: ${props.fontColor};
+//     `
+//     const Header = styled.h1`
+//     font-size: ${props.fontSize}px;
+//     color: ${props.fontColor};
+//     `
+//     const Form = styled.form`
+//     background-color: ${background};
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     padding-bottom: 5%;
+//     `
 
-    const Input = styled.input`
-    font-size: ${props.fontSize - 10}px;
-    `
+//     const Input = styled.input`
+//     font-size: ${props.fontSize - 10}px;
+//     `
 
 
-    console.log("this is the form", form);
-    const handleSubmit = e => {
-        e.preventDefault();
-        props.signUp(form);
-        props.history.push("/home");
-    };
-    const handleChanges = e => {
-        setForm({ ...form, [e.target.name]: e.target.value });
-    };
+//     console.log("this is the form", form);
+//     const handleSubmit = e => {
+//         e.preventDefault();
+//         props.signUp(form);
+//         props.history.push("/home");
+//     };
+//     const handleChanges = e => {
+//         setForm({ ...form, [e.target.name]: e.target.value });
+//     };
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Header>
-                Join Our ACS Team!
-          </Header>
+//     return (
+//         <Form onSubmit={handleSubmit}>
+//             <Header>
+//                 Join Our ACS Team!
+//           </Header>
 
-            <div className="register-title">
-                <h3>Your Information</h3>
-            </div>
-            <Label> Full Name</Label>
-            <Input
-                type="text"
-                name="full_name"
-                placeholder="Full Name"
-                value={form.name}
-                onChange={handleChanges}
-            />
-            <Label> Location (State) </Label>
-            <Input
-                type="location"
-                name="location"
-                placeholder="Location(state)"
-                value={form.location}
-                onChange={handleChanges}
-            />
-            <Label>Email </Label>
-            <Input
-                type="email"
-                name="email"
-                placeholder="Email"
-                value={form.email}
-                onChange={handleChanges}
-            />
+//             <div className="register-title">
+//                 <h3>Your Information</h3>
+//             </div>
+//             <Label> Full Name</Label>
+//             <Input
+//                 type="text"
+//                 name="full_name"
+//                 placeholder="Full Name"
+//                 value={form.name}
+//                 onChange={handleChanges}
+//             />
+//             <Label> Location (State) </Label>
+//             <Input
+//                 type="location"
+//                 name="location"
+//                 placeholder="Location(state)"
+//                 value={form.location}
+//                 onChange={handleChanges}
+//             />
+//             <Label>Email </Label>
+//             <Input
+//                 type="email"
+//                 name="email"
+//                 placeholder="Email"
+//                 value={form.email}
+//                 onChange={handleChanges}
+//             />
 
-            <Label> Username </Label>
-            <Input
-                type="text"
-                name="username"
-                placeholder="Username"
-                value={form.username}
-                onChange={handleChanges}
-            />
-            <Label> Password </Label>
-            <Input
-                type="password"
-                name="password"
-                placeholder="Password"
-                value={form.password}
-                onChange={handleChanges}
-            />
-            <div>
-                <button type="submit">Sign Up</button>
+//             <Label> Username </Label>
+//             <Input
+//                 type="text"
+//                 name="username"
+//                 placeholder="Username"
+//                 value={form.username}
+//                 onChange={handleChanges}
+//             />
+//             <Label> Password </Label>
+//             <Input
+//                 type="password"
+//                 name="password"
+//                 placeholder="Password"
+//                 value={form.password}
+//                 onChange={handleChanges}
+//             />
+//             <div>
+//                 <button type="submit">Sign Up</button>
 
-                <Link
-                    style={{
-                        textDecoration: "none",
-                        color: "#FE0707",
-                        fontSize: "14px",
-                        fontWeight: "bold"
-                    }}
-                    to="/login"
-                >
-                    Already have an Account?
-            </Link>
-            </div>
-        </Form>
-    );
-};
-const mapStateToProps = state => {
-    return state;
-};
+//                 <Link
+//                     style={{
+//                         textDecoration: "none",
+//                         color: "#FE0707",
+//                         fontSize: "14px",
+//                         fontWeight: "bold"
+//                     }}
+//                     to="/login"
+//                 >
+//                     Already have an Account?
+//             </Link>
+//             </div>
+//         </Form>
+//     );
+// };
+// const mapStateToProps = state => {
+//     return state;
+// };
 
-export default connect(mapStateToProps, { signUp })(CreateAccount);
+// export default connect(mapStateToProps, { signUp })(CreateAccount);
