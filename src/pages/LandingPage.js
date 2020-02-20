63cbe313a6ef968b0933dd2d0d6d68f8920463d9
Taylor Cool <taycool@gmail.com>
2020-02-19 16:07:24 -0800

    styling landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Wrapper, Box, Flex, AppWrapper } from 'adaptiv-ui';
+import landingImage from '../images/landingImage.jpeg';
 
 
 
 const LandingPage = () => {
+
+  return (
   
-    return (
-        <div className= "API-message">
-          <h1>Let's get it started!!!</h1>
-        </div>
-    )
+    <Wrapper>
+      <AppWrapper bg_src={landingImage} >
+        <Flex w='60%' h='70vh' >
+          <h1 className='hero-text'>Your Home for Angel City Sports Events and More!</h1>
+        </Flex>
+      </AppWrapper>
+    </Wrapper>
+  )
 }
 
 export default LandingPage
