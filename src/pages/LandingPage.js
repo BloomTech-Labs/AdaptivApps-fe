4da0724e3b0ea7c8c6763ef5553e3791671d1802
Taylor Cool <taycool@gmail.com>
2020-02-20 08:58:35 -0800

    landing page styled

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,23 +1,52 @@
 import React from 'react';
-import { Wrapper, Box, Flex, AppWrapper } from 'adaptiv-ui';
+import { Wrapper, Box, Flex, Container, Button } from 'adaptiv-ui';
 import landingImage from '../images/landingImage.jpeg';
+import { FaFacebookSquare, FaGoogle } from 'react-icons/fa'
+import { IconContext } from 'react-icons';
 
 
 
 const LandingPage = () => {
 
   return (
-  // NavBar here, when sidebar nav is built
+    <IconContext.Provider value={{ color: 'white', size: '3rem' }}>
+  {/* NavBar here, when sidebar nav is built */}
     <Wrapper>
-      <AppWrapper bg_src={landingImage} >
+      <Container bg_src={landingImage} invert attach_fix >
         <Flex h='70vh' jc_center ai_center >
           <Flex w='85%' jc_center ai_center >
             <h1 className='hero-text'>Your Home for Angel City Sports Events and More!</h1>
           </Flex>
         </Flex>
-      </AppWrapper>
-      
+      </Container>
+      <Box h='6vh' />
+      <Flex drape >
+        <Flex>
+          <h2>Sign Up Now!</h2>
+        </Flex>
+        <Flex jc_center ai_center>
+          <Flex drape w='30%' >
+            <p>Sign Up Now with Facebook or Google, add your profile info, and keep track of Angel City Sports Games, Clinics, and other events! All the info you need is all in one place - The Angel City Sports App.</p>
+          </Flex>
+          <Flex drape w='30%' >
+            <Button bg='#3B5998' secondary w='25rem' >
+              <Flex jc_between ai_center >
+                <FaFacebookSquare /> 
+                <p>Sign up with Facebook</p>
+              </Flex>
+            </Button>
+            <Button secondary w='25rem' >
+              <Flex jc_between ai_center >
+                <FaGoogle /> 
+                <p>Sign up with Google</p>
+              </Flex>
+            </Button>
+          </Flex>
+        </Flex>
+        <Box h='8vh' />
+      </Flex>
     </Wrapper>
+    </IconContext.Provider>
   )
 }
 
