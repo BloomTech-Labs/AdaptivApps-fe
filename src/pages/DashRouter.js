7e4067c0c12e66e166e75981c714c12e2230788a
Robert Campbell <Campbell.Rob@protonmail.com>
2020-02-20 09:41:39 -0800

    rename Profile.js to DashRouter.js, deleted unused code

---
+++ src/pages/DashRouter.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useAuth0 } from "../components/auth/react-auth0-spa";
+import config from "../components/auth/auth_config.json";
+import AdminDashboard from "./admin/AdminDashboard";
+import UserDashboard from "./users/UserDashboard";
+import { Flex, Box } from "adaptiv-ui";
+
+// example Profile page
+
+const DashRouter = () => {
+  const { loading, user } = useAuth0();
+  console.log(user);
+
+  // loading and no user will show Loading div
+  if (loading || !user) {
+    return <div>Loading...</div>;
+  }
+
+  return user[config.roleUrl].includes("Admin") ? (
+    <div>
+      <AdminDashboard user={user} />
+    </div>
+  ) : (
+    <div>
+      <UserDashboard user={user} />
+    </div>
+  );
+};
+
+export default DashRouter;
