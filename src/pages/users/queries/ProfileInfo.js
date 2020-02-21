bc93f951345a9a4668c3548371d96f49644c9016
Robert Campbell <Campbell.Rob@protonmail.com>
2020-02-20 16:30:48 -0800

    profile successfully renders profile information

--- src/pages/users/queries/ProfileInfo.js
+++ src/pages/users/queries/ProfileInfo.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { useQuery } from "@apollo/react-hooks";
-import { gql } from "apollo-boost";
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
 //Query used to obtain user profile information
 const PROFILE_INFO = gql`
   {
-    profile {
+    profile(where: { id: "ck6veiafj000l0858kc2mb547" }) {
       email
       firstName
       lastName
@@ -26,35 +26,21 @@ function ProfileInfo() {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-
-  return data.profile(
-    ({
-      email,
-      firstName,
-      lastName,
-      displayName,
-      birthday,
-      bio,
-      disability,
-      legal,
-      createdAt,
-      updatedAt
-    }) => (
-      <main key={data.id}>
-        <details>
-          <p>{email}</p>
-          <p>{firstName}</p>
-          <p>{lastName}</p>
-          <p>{displayName}</p>
-          <p>{birthday}</p>
-          <p>{bio}</p>
-          <p>{disability}</p>
-          <p>{legal}</p>
-          <p>{createdAt}</p>
-          <p>{updatedAt}</p>
-        </details>
-      </main>
-    )
+  return (
+    <main>
+      <div>
+        <p>{data.profile.email}</p>
+        <p>{data.profile.firstName}</p>
+        <p>{data.profile.lastName}</p>
+        <p>{data.profile.displayName}</p>
+        <p>{data.profile.birthday}</p>
+        <p>{data.profile.bio}</p>
+        <p>{data.profile.disability}</p>
+        <p>{data.profile.legal}</p>
+        <p>{data.profile.createdAt}</p>
+        <p>{data.profile.updatedAt}</p>
+      </div>
+    </main>
   );
 }
 
