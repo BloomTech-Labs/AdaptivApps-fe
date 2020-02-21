b84ec16d5de9b1bfb223beef17305a7f37a4f0eb
Taylor Cool <taycool@gmail.com>
2020-02-21 13:57:07 -0800

    dashbaord style started

--- src/pages/users/UserDashboard.js
+++ src/pages/users/UserDashboard.js
@@ -1,17 +1,31 @@
 import React from 'react'
+import { useAuth0 } from "../../components/auth/react-auth0-spa";
 import config from "../../components/auth/auth_config.json";
+import { Flex, Box } from "adaptiv-ui"
 
 function UserDashboard(props) {
   const { user } = props;
-  return (
-    <div>
-      <img src={user.picture} alt="Profile" />
-      <h1>This is USER </h1>
-      <h2>{user.name}</h2>
-      <h3>{user[config.roleUrl]}</h3>
-      <p>{user.email}</p>
-      <code>{JSON.stringify(user, null, 2)}</code>
-    </div>
+  const { loading } = useAuth0();
+  // console.log(user)
+
+  // loading and no user will show Loading div
+  if (loading || !user) {
+    return <div>Loading...</div>;
+  } else
+    return(
+      <Flex ai_start col>
+        <h2>Account Information</h2>
+        <Box h='0.2rem' w='90%' bg='grey' />
+        <Flex ai_center>
+        <Box sqr='5rem' >
+          <img src={user.picture} alt="Profile" />
+        </Box>
+        <h2>{user.name} ({user[config.roleUrl]})</h2>
+        </Flex>
+        <p>{user.email}</p>
+      
+        <code>{JSON.stringify(user, null, 2)}</code>
+      </Flex>
   )
 }
 
