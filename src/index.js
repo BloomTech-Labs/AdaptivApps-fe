df15b6383cfec9dc455627565877469ef3a569af
Robert Campbell <Campbell.Rob@protonmail.com>
2020-02-24 11:28:13 -0800

    class component for auth0 token

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
-import * as auth0 from './components/auth/react-auth0-spa';
+import auth from './components/auth/Auth';
+// import * as auth0 from './components/auth/react-auth0-spa';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { AppWrapper } from 'adaptiv-ui';
 import 'adaptiv-ui/css/main.css';
@@ -11,18 +12,19 @@ import * as serviceWorker from './serviceWorker';
 
 //pointing apollo client to the apollo api.
 
-console.log('all exports from react-auth0-rsa', auth0);
+// console.log('all exports from react-auth0-rsa', auth0);
 const client = new ApolloClient({
   uri: 'http://localhost:8000',
   request: operation => {
     operation.setContext(context => ({
       headers: {
         ...context.headers,
-        authorization: auth0,
+        authorization: auth.getIdToken(),
       },
     }));
   },
 });
+
 //!!pointing apollo client to the apollo api.
 
 ReactDOM.render(
