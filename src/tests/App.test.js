e00da78d7870258ae14691c87ba72e6fdb6ff074
Jimmy McBride <46766874+JimmyMcBride@users.noreply.github.com>
2020-02-27 09:58:58 -0600

    added passing test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDom from 'react-dom';
+// import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders app', () => {
+  const div = document.createElement('div');
+  ReactDom.render(<App />, div);
 });
