f37aa6bbb675a2bcac60369563980cb11eea3f39
JimmyMcBride <mcbride967@gmail.com>
2020-02-27 07:27:03 -0600

    cleaned up linter

--- src/pages/admin/AdminDashboard.js
+++ src/pages/admin/AdminDashboard.js
@@ -1,8 +1,18 @@
-import React, { useRef } from 'react'
-import config from "../../components/auth/auth_config.json";
-import { useAuth0 } from "../../components/auth/react-auth0-spa";
+import React from 'react';
+import config from '../../components/auth/auth_config.json';
+import { useAuth0 } from '../../components/auth/react-auth0-spa';
 import PropTypes from 'prop-types';
-import { Flex, Box, Text, Button, Form, Input, theme, TextArea, Select } from "adaptiv-ui";
+import {
+  Flex,
+  Box,
+  Text,
+  Button,
+  Form,
+  Input,
+  theme,
+  TextArea,
+  Select,
+} from 'adaptiv-ui';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
@@ -15,13 +25,13 @@ const ProfileSchema = yup.object().shape({
   // bio: yup.string(),
   // disability: yup.string(), //string?
   // legal: yup.bool()
-})
+});
 
 function AdminDashboard(props) {
   const { user } = props;
   const { loading } = useAuth0();
   // console.log(user)
-  const { handleSubmit, register, errors } = useForm({
+  const { handleSubmit, register } = useForm({
     mode: 'onSubmit',
     validationSchema: ProfileSchema,
     defaultValues: {
@@ -29,11 +39,11 @@ function AdminDashboard(props) {
       firstName: user.given_name,
       lastName: user.family_name,
       displayName: user.nickname,
-    }
+    },
   });
-  const userRegister = useRef(register({}))
   // set editing state for email change button?
-  
+
+  // eslint-disable-next-line
   const onSubmit = (data, e) => {
     // console.log('hello world');
     // console.log(data, e);
@@ -45,134 +55,160 @@ function AdminDashboard(props) {
   if (loading || !user) {
     return <div>Loading...</div>;
   } else
-    return(
+    return (
       <Flex ai_start col stretch>
+        <Text xlf bold mm>
+          Account Information
+        </Text>
 
-        <Text xlf bold mm>Account Information</Text>
-        
-        <Box h='0.2rem' w='90%' bg='lightgrey' />
-        
+        <Box h="0.2rem" w="90%" bg="lightgrey" />
+
         <Flex mm col ai_start>
           {/* <Box h='5rem' /> */}
           <Flex jc_between ai_center>
-
-            <Box sqr='5rem' >
+            <Box sqr="5rem">
               <img src={user.picture} alt="Profile" />
             </Box>
-            <Text lf sm>{user.name} ({user[config.roleUrl]})</Text>
-
+            <Text lf sm>
+              {user.name} ({user[config.roleUrl]})
+            </Text>
           </Flex>
 
-          <Box h='2rem' />
+          <Box h="2rem" />
 
           <Flex ai_start col>
-
             <Text lf bold>
               Account Email Address
             </Text>
 
             <Flex ai_center>
-              <Text lf>
-                {user.email}
-              </Text>
-              <Button primary jc_center mm border={`2px solid ${theme.primary}`} w='7.75rem' h='3.75rem' aria-label='Change email for this user' onClick={() => alert('functionality coming next release canvas')}>Change</Button>     
+              <Text lf>{user.email}</Text>
+              <Button
+                primary
+                jc_center
+                mm
+                border={`2px solid ${theme.primary}`}
+                w="7.75rem"
+                h="3.75rem"
+                aria-label="Change email for this user"
+                onClick={() =>
+                  alert('functionality coming next release canvas')
+                }
+              >
+                Change
+              </Button>
             </Flex>
-     
           </Flex>
 
           <Form ai_start col stretch onSubmit={handleSubmit(onSubmit)}>
-            
             <Text xlf bold>
               Personal Information
             </Text>
-     
-            <Box h='2rem' />
-     
+
+            <Box h="2rem" />
+
             <Flex jc_between stretch>
-     
               <Flex ai_start col>
-                <Text mf>
-                  First Name
-                </Text>
+                <Text mf>First Name</Text>
                 <Flex ai_center>
-                  <Input type='text' placeholder={user.given_name} w='25rem' name='firstName' ref={register} />
+                  <Input
+                    type="text"
+                    placeholder={user.given_name}
+                    w="25rem"
+                    name="firstName"
+                    ref={register}
+                  />
                   {/* <input type='text' placeholder={user.given_name} w='25rem' name='firstName' ref={register} /> */}
                 </Flex>
               </Flex>
 
               <Flex ai_start col>
-                <Text mf>
-                  Last Name
-                </Text>
+                <Text mf>Last Name</Text>
                 <Flex ai_center>
-                  <Input type='text' placeholder={user.family_name} w='25rem' name='lastName' ref={register} />
+                  <Input
+                    type="text"
+                    placeholder={user.family_name}
+                    w="25rem"
+                    name="lastName"
+                    ref={register}
+                  />
                 </Flex>
               </Flex>
-
             </Flex>
 
             <Flex jc_between stretch>
               <Flex ai_start col>
-                <Text mf>
-                  Display Name
-                </Text>
+                <Text mf>Display Name</Text>
                 <Flex ai_center>
-                  <Input type='text' placeholder={user.nickname} w='25rem' name='displayName' ref={register} />
+                  <Input
+                    type="text"
+                    placeholder={user.nickname}
+                    w="25rem"
+                    name="displayName"
+                    ref={register}
+                  />
                 </Flex>
               </Flex>
 
               <Flex ai_start col>
-                <Text mf>
-                  Date of Birth
-                </Text>
+                <Text mf>Date of Birth</Text>
                 <Flex ai_center>
-                  <Input type='date' w='25rem' name='birthday' ref={register} />
+                  <Input type="date" w="25rem" name="birthday" ref={register} />
                 </Flex>
               </Flex>
             </Flex>
-            
+
             <Flex ai_start col stretch>
-              <Text mf>
-                Bio
-              </Text>
-              <TextArea rows='8' cols='60' name='bio' ref={register} />
+              <Text mf>Bio</Text>
+              <TextArea rows="8" cols="60" name="bio" ref={register} />
             </Flex>
 
             <Flex jc_between stretch>
-     
               <Flex ai_start col>
                 <Text mf>
                   Disability Status {/* is there a better way to word this? */}
                 </Text>
                 <Flex ai_center>
-                  <Input type='select' placeholder='' w='25rem' name='disability' ref={register} />
+                  <Input
+                    type="select"
+                    placeholder=""
+                    w="25rem"
+                    name="disability"
+                    ref={register}
+                  />
                 </Flex>
               </Flex>
 
               <Flex ai_start col>
-                <Text mf>
-                  Are you over 18 years old?
-                </Text>
+                <Text mf>Are you over 18 years old?</Text>
                 <Flex ai_center>
-                  <Select w='20rem' name='legal' ref={register} >
+                  <Select w="20rem" name="legal" ref={register}>
                     <option value={true}>Yes</option>
                     <option value={false}>No</option>
                   </Select>
                 </Flex>
               </Flex>
-
             </Flex>
 
-            <Button type='submit' jc_center primary border={`2px solid ${theme.primary}`} w='9rem' h='4rem' aria-label='save changes to user profile' >Save</Button>
+            <Button
+              type="submit"
+              jc_center
+              primary
+              border={`2px solid ${theme.primary}`}
+              w="9rem"
+              h="4rem"
+              aria-label="save changes to user profile"
+            >
+              Save
+            </Button>
           </Form>
-
         </Flex>
         {/* <Flex drape mm mp>
         
           <code>{JSON.stringify(user, null, 2)}</code>
         </Flex> */}
       </Flex>
-  )
+    );
 }
 
 export default AdminDashboard;
