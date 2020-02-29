f59957109a777e4b63cadd5a396f29f5a47ef55e
JimmyMcBride <mcbride967@gmail.com>
2020-02-29 15:06:27 -0600

    Fixed TextArea component and did some ESLint clean up.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -10,7 +10,7 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
-import process from 'process';
+const process = require('process');
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
