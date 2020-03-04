b8e6e0519bf3f9a627843a5a1c2c8a645e6f51f6
Robert Campbell <Campbell.Rob@protonmail.com>
2020-03-04 10:53:09 -0800

    getting list of events

--- src/pages/DashRoute/users/events/EventList.js
+++ src/pages/DashRoute/users/events/EventList.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import SideNav from '../../../../components/SideNav';
 
+import { useQuery } from 'react-apollo';
+import { GET_EVENT_LIST } from '../../queries/events/getEvents';
+
 import { useAuth0 } from '../../../../components/auth/react-auth0-spa';
 // Styling imports
 import { Flex, Box } from 'adaptiv-ui';
 
 export default function EventList() {
+  const { loading, error, data } = useQuery(GET_EVENT_LIST);
   const { user } = useAuth0();
+  console.log(data);
   return (
     <Flex>
       <Box w="15vw" min_w="25rem">
         <SideNav user={user} />
       </Box>
-      <p>event list is working</p>
+      <EventCard />
     </Flex>
   );
 }
