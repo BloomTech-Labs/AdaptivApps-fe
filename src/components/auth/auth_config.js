249ceca564209bb183001e0de15d5607df9c629b
Maddy <mmcintosh2k14@gmail.com>
2020-03-05 21:15:17 -0500

    edits variable name in env and auth_config.js

--- src/components/auth/auth_config.js
+++ src/components/auth/auth_config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  domain: process.env.REACT_APP_DOMAIN,
+  domain: process.env.REACT_APP_CLIENT_DOMAIN,
   clientId: process.env.REACT_APP_CLIENT_ID,
   roleUrl: 'http://adaptivapps.com/roles',
   audience: process.env.REACT_APP_AUDIENCE,
