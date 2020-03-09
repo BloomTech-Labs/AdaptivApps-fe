cde464634352d8e278a942330eea56c07ed6f467
JonathanChen <jonny29009@yahoo.com>
2020-03-09 13:00:56 -0700

    adding createEvent and updateEvent mutation

---
+++ src/pages/CreateEvent/queries/index.js
@@ -0,0 +1,45 @@
+import gql from 'graphql-tag';
+
+// Creating an event
+export const CREATE_EVENT = gql`
+  mutation CreateEvent(
+    $title: String
+    $startDate: String
+    $endDate: String
+    $location: String
+  ) {
+    createEvent(
+      data: {
+        title: $title
+        startDate: $startDate
+        endDate: $endDate
+        location: $location
+      }
+    ) {
+      title
+      startDate
+      endDate
+      location
+    }
+  }
+`;
+
+// Updating an event
+export const UPDATE_EVENT = gql`
+  mutation UpdateEvent($id: String!) {
+    updateEvent(
+      data: {
+        title: $title
+        startDate: $startDate
+        endDate: $endDate
+        location: $location
+      }
+      where: { id: $id }
+    ) {
+      title
+      startDate
+      endDate
+      location
+    }
+  }
+`;
