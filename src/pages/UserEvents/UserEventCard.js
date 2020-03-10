75634b3d9e375d04954fa981dbbe7516b98d4692
Maddy <mmcintosh2k14@gmail.com>
2020-03-10 16:35:56 -0400

    creates UserEventCard.js

---
+++ src/pages/UserEvents/UserEventCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import golfimg from '../../assets/images/little-guy-golf.jpg';
+import { Flex, Container } from 'adaptiv-ui';
+import PropTypes from 'prop-types';
+
+export default function UserEventCard({ event }) {
+  return (
+    <Flex col>
+      <Container bg_src={golfimg} h="10vh" w="20rem"></Container>
+      <small>
+        {event.startDate} - {event.endDate}
+      </small>
+      <h6>
+        <b>{event.title}</b>
+      </h6>
+      <p>location</p>
+    </Flex>
+  );
+}
+
+EventCard.propTypes = {
+  event: PropTypes.object,
+};
