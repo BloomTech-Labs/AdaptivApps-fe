a49a2190f76069b7a853bd1c129184a7d11b353c
Robert Campbell <Campbell.Rob@protonmail.com>
2020-03-10 12:58:13 -0700

    updated cors

--- src/pages/EventsCalendar/EventCard.js
+++ src/pages/EventsCalendar/EventCard.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import golfimg from '../../assets/images/little-guy-golf.jpg';
-import { Flex, Container } from 'adaptiv-ui';
+import {
+  Flex,
+  Container,
+  Button,
+  Modal,
+  useModal,
+  Text,
+  NavLink,
+  Linkton,
+} from 'adaptiv-ui';
 import PropTypes from 'prop-types';
 
 export default function EventCard({ event }) {
+  const [isActive, toggle] = useModal();
   return (
     <Flex col>
       <Container bg_src={golfimg} h="10vh" w="20rem"></Container>
@@ -13,7 +23,34 @@ export default function EventCard({ event }) {
       <h6>
         <b>{event.title}</b>
       </h6>
-      <p>location</p>
+      <p>{event.location}</p>
+      <Button primary onClick={toggle} w="20rem">
+        Add to my schedule
+      </Button>
+      <Modal isActive={isActive} toggle={toggle}>
+        <Flex w="40rem" h="40rem" drape>
+          <small>
+            {event.startDate} - {event.endDate}
+          </small>
+          <h6>
+            <b>{event.title}</b>
+          </h6>
+          <p>{event.location}</p>
+          <Text>
+            Join us in June 2020 for the 6th annual Angel City Games, presented
+            by The Hartford, the largest Paralympic-style adaptive sports event
+            in the Western US! We’ve added more sports to the schedule this year
+            and anticipate hosting our largest event ever - Don’t miss it!
+          </Text>
+          <Text> Add to "My Events?"</Text>
+          <NavLink primary autoFocus to="activities">
+            Click me!
+          </NavLink>
+          <Button secondary onClick={toggle}>
+            Close
+          </Button>
+        </Flex>
+      </Modal>
     </Flex>
   );
 }
