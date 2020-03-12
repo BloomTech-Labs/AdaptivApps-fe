c3b4977bd7ea8b8c190fbdc2f25cea9102ffa89d
JonathanChen <jonny29009@yahoo.com>
2020-03-12 13:11:49 -0700

    add more to activitycards

--- src/pages/AddActivity/ActivityCard.js
+++ src/pages/AddActivity/ActivityCard.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default function ActivityCard(props) {
   return (
     <div>
-      <p>Name is: {props.activity.name}</p>
+      <p>Name: {props.activity.name}</p>
+      <p>Date: {props.activity.startDate}</p>
+      <p>Location: {props.activity.location}</p>
+      <p>Time: {props.activity.startTime}</p>
     </div>
   );
 }
