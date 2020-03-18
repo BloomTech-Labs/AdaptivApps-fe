f643b3b273a5f35910dc4ce190fcd01dfa052477
Forrest Bingham <bingham_5@live.com>
2020-03-18 17:09:44 -0600

    Added functionality for volunteers and other

--- src/pages/ActivitiesList/Activities.js
+++ src/pages/ActivitiesList/Activities.js
@@ -3,6 +3,8 @@ import {ToolTip, NavLink, Text, Flex, Button, Modal, useModal} from "adaptiv-ui"
 import { useMutation } from 'react-apollo';
 import { REGISTER_AS_ATHLETE } from './queries/AthleteRegister';
 import { REGISTER_AS_COACH } from './queries/CoachRegister';
+import { REGISTER_AS_VOLUNTEER } from './queries/VolunteerRegister';
+import { REGISTER_AS_OTHER } from './queries/OtherRegister';
 import { useAuth0 } from '../../config/react-auth0-spa';
 import { IconContext } from 'react-icons';
 import { IoIosAddCircle } from "react-icons/io";
@@ -12,7 +14,7 @@ import PropTypes from 'prop-types';
 export default function Activities({ activity }) {
 
   const [clicked, setClicked] = useState(false);
-  const [color, setColor] = useState("black");
+  const [color] = useState("black");
 
   function toggle() {
     setClicked(!clicked);
@@ -27,6 +29,8 @@ export default function Activities({ activity }) {
 
   const [registerAsAthlete] = useMutation(REGISTER_AS_ATHLETE)
   const [registerAsCoach] = useMutation(REGISTER_AS_COACH)
+  const [registerAsVolunteer] = useMutation(REGISTER_AS_VOLUNTEER)
+  const [registerAsOther] = useMutation(REGISTER_AS_OTHER)
 
   const { user } = useAuth0();
 
@@ -44,25 +48,40 @@ export default function Activities({ activity }) {
     alert("Successfully registered as a Coach!")
   }
 
+  const volunteerRegister = async () => {
+    await registerAsVolunteer({
+      variables: {id:activity.id, email:user.email}
+    })
+    alert("Successfully registered as a Volunteer")
+  }
+
+  const otherRegister = async () => {
+    await registerAsOther({
+      variables: {id: activity.id, email:user.email}
+    })
+    alert("Successfully registered as a Spectator")
+  }
+
   return (
-    <div>
-      <table>
+    <Flex row>
+    <div >
+      <table border="1px solid black">
         <thead>
           <tr>
-            <th>Name</th>
+            <th w="30%">Name</th>
             <th>Date</th>
             <th>Location</th>
             <th>Time</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody w="100%">
           <tr>
             <td>{activity.name}</td>
             <td>{activity.startDate}</td>
             <td>{activity.location}</td>
             <td>{activity.startTime}</td>
             <td>
-              <ToolTip tt_left="50%" tabIndex={0} tt_w="14rem" border="none">
+              <ToolTip tt_left="50%" tabIndex={0} tt_w="14rem" border="none" position="absolute">
                   <IconContext.Provider value={ clicked ? {style: {background:'white', color: "#FFC629" , fontSize:"3rem"}} : {style: {background:'white', color: "#2962FF", fontSize:"3rem"}} }>
                   <Button bg={"white"} onClick={toggle} border="none">
                   <IoIosAddCircle/>
@@ -72,28 +91,36 @@ export default function Activities({ activity }) {
 
               
             </td>
-            {clicked ? (
-            <Flex col>
-                  <Button bg="white" onFocus={hoverEnter} onBlur={hoverLeave} color={color} onMouseOver={hoverEnter} onMouseLeave={hoverLeave} onClick={(()=> console.log('clicked', athleteRegister, activity.id, user.email), athleteRegister)}>
+            <td>
+            <div border="1px solid black" >
+              {clicked ? (
+            <Flex col f_size="2rem">
+                  <Button m=".2rem" p=".2rem" bg="cyan" xsf ta_left onFocus={hoverEnter} onBlur={hoverLeave} color={color} onMouseOver={hoverEnter} onMouseLeave={hoverLeave} onClick={(()=> console.log('clicked'), athleteRegister)}>
                     I'm Competing
                     </Button>
-                    <Button bg="white" onFocus={hoverEnter} onBlur={hoverLeave} color={color} onMouseOver={hoverEnter} onMouseLeave={hoverLeave} onClick={(()=> console.log('clicked'), coachRegister)}>
+                    <Button  m=".2rem" p=".2rem" bg="cyan" xsf ta_left onFocus={hoverEnter} onBlur={hoverLeave} color={color} onMouseOver={hoverEnter} onMouseLeave={hoverLeave} onClick={(()=> console.log('clicked'), coachRegister)}>
                     I'm Coaching
                     </Button>
-                    <Text>
+                    <Button m=".2rem" p=".2rem" bg="cyan" xsf ta_left onFocus={hoverEnter} onBlur={hoverLeave} color={color} onMouseOver={hoverEnter} onMouseLeave={hoverLeave} onClick={(()=> console.log('clicked'), volunteerRegister)}>
                     I'm Volunteering
-                    </Text>
-                    <Text>
+                    </Button>
+                    <Button m=".2rem" p=".2rem" bg="cyan" xsf ta_left onFocus={hoverEnter} onBlur={hoverLeave} color={color} onMouseOver={hoverEnter} onMouseLeave={hoverLeave} onClick={(()=> console.log('clicked'), otherRegister)}>
                     I'm Spectating
-                    </Text>
+                    </Button>
                     </Flex>
                     ) : null}
-          </tr>
+               </div>
+            </td>
 
+          </tr>
+          
+       
+          
         </tbody>
         
       </table>
     </div>
+    </Flex>
   );
 }
 Activities.propTypes = {
