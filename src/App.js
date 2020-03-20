c7f4d9f7f49aabce63e5fd6e79949cbe18d3b808
JonathanChen <jonny29009@yahoo.com>
2020-03-20 11:33:21 -0700

    removing token comment

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
     credentials: 'same-origin',
     request: async operation => {
       const token = await getIdTokenClaims();
-      console.log(token);
+      // console.log(token);
       // Attach token to header
       operation.setContext(context => ({
         headers: {
