0c4c4160fa2cd8a28007a7332e844bc2b5be8dd1
Maddy <mmcintosh2k14@gmail.com>
2020-03-19 20:18:16 -0400

    accepts incoming changes from pull from master

---
+++ src/pages/Landing/Legal/Accessibility.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+export default function Accessibility() {
+  return (
+    <>
+      <div className="access-wrapper">
+        <h1>Accessibility Statement</h1>
+        <br />
+        <br />
+        <p>Updated: March 2020.</p>
+
+        <br />
+        <br />
+        <h2>General</h2>
+        <p>
+          adaptivdev.net strives to ensure that its services are accessible to
+          people with disabilities. adaptivdev.net has invested a significant
+          amount of resources to help ensure that its website is made easier to
+          use and more accessible for people with disabilities, with the strong
+          belief that every person has the right to live with dignity, equality,
+          comfort and independence.
+        </p>
+
+        <h2>Accessibility on adaptivdev.net</h2>
+        <p>
+          adaptivdev.net makes available the{' '}
+          <a
+            href="https://UserWay.org"
+            alt="Free Website Accessibility Widget"
+            title="Free Website Accessibility Widget"
+          >
+            UserWay Website Accessibility Widget
+          </a>{' '}
+          that is powered by a dedicated accessibility server. The software
+          allows adaptivdev.net to improve its compliance with the Web Content
+          Accessibility Guidelines (WCAG 2.1).
+        </p>
+
+        <h2>Enabling the Accessibility Menu</h2>
+        <p>
+          The adaptivdev.net accessibility menu can be enabled by clicking the
+          accessibility menu icon that appears on the corner of the page. After
+          triggering the accessibility menu, please wait a moment for the
+          accessibility menu to load in its entirety.
+        </p>
+
+        <h2>Disclaimer</h2>
+        <p>
+          adaptivdev.net continues its efforts to constantly improve the
+          accessibility of its site and services in the belief that it is our
+          collective moral obligation to allow seamless, accessible and
+          unhindered use also for those of us with disabilities.
+        </p>
+
+        <p>
+          Despite our efforts to make all pages and content on adaptivdev.net
+          fully accessible, some content may not have yet been fully adapted to
+          the strictest accessibility standards. This may be a result of not
+          having found or identified the most appropriate technological
+          solution.
+        </p>
+
+        <h2>Here For You</h2>
+        <p>
+          If you are experiencing difficulty with any content on adaptivdev.net
+          or require assistance with any part of our site, please contact us
+          during normal business hours as detailed below and we will be happy to
+          assist.
+        </p>
+
+        <h2>Contact Us</h2>
+        <p>
+          If you wish to report an accessibility issue, have any questions or
+          need assistance, please contact adaptivdev.net Customer Support as
+          follows:
+        </p>
+
+        <br />
+        <br />
+
+        <p>
+          Email:{' '}
+          <a href="mailto:admin@adaptivapps.org">admin@adaptivapps.org</a>
+        </p>
+      </div>
+    </>
+  );
+}
