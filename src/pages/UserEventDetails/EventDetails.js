ea44f189d9d999a04a3d873f47b8e48cec6672e1
Maddy <mmcintosh2k14@gmail.com>
2020-03-20 20:36:45 -0400

    displays user registered activity

--- src/pages/UserEventDetails/EventDetails.js
+++ src/pages/UserEventDetails/EventDetails.js
@@ -1,11 +1,18 @@
 // React imports
 import React from 'react'
+import Activities from '../ActivitiesList/Activities';
 
 
 export default function EventDetails({ event }) {
+  console.log(event.activities);
   return (
     <div>
       <h5>{event.title}</h5>
+      {event.activities &&
+        event?.activities?.map((activity, id) => (
+          <Activities key={id} activity={activity} />
+        ))}
+      
     </div>
   )
 }
