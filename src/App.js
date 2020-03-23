9997270d1a789747ca91eea7a275429308d3e882
Robert Campbell <38469375+Robert-D-Campbell@users.noreply.github.com>
2020-03-23 09:41:17 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,6 @@ function App() {
     credentials: 'same-origin',
     request: async operation => {
       const token = await getIdTokenClaims();
-      console.log("use this", token.__raw);
       // Attach token to header
       operation.setContext(context => ({
         headers: {
