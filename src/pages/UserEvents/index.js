dd4e2f344bc77681c56ecaabadbbdcd26ef9c50d
MsMaddyMac <mmcintosh2k14@gmail.com>
2020-04-02 21:00:44 -0400

    fixes card width bug in MyEventCard

--- src/pages/UserEvents/index.js
+++ src/pages/UserEvents/index.js
@@ -8,9 +8,34 @@ import { GET_USER_EVENTS } from './queries';
 // Auth0 imports
 import { useAuth0 } from '../../config/react-auth0-spa';
 // Styling imports
-import { Flex, Box } from 'adaptiv-ui';
+import {
+  makeStyles,
+  Grid,
+  Box,
+  Typography,
+} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: '100%',
+    width: '90%',
+    fontFamily: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+  },
+  headingBox: {
+    margin: '6rem 0 2rem 3rem',
+    fontWeight: '400',
+    borderColor: '#D3D3D3',
+  },
+  grid: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flexWrap: 'wrap',
+    marginLeft: '3rem',
+  },
+});
 
 export default function UserEvents() {
+  const classes = useStyles();
   // Retrieves logged in user info
   const { user } = useAuth0();
   // Retrieves all events a user is registered to
@@ -25,15 +50,18 @@ export default function UserEvents() {
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
   return (
-    <Flex ai_start col stretch style={{marginLeft: "1rem", marginTop: "4rem"}}>
-      <h4 style={{margin: '1rem 0rem 0.8rem 1.5rem', fontSize: "2.4rem"}}>My Events</h4>
-      <Box h="0.2rem" w="90%" bg="lightgrey" m="0 0 0 1.6rem" />
-      <Flex jc_between row w="90%" style={{ flexWrap: "wrap" }}>
-      {data &&
-        data.events.map((event, id) => (
-          <MyEventCard refetch={refetch} key={id} event={event} />
-        ))}
-      </Flex>
-    </Flex>
+    <main className={classes.root}>
+      <Box className={classes.headingBox} borderBottom={2}>
+        <Typography className={classes.heading} variant="h3" gutterBottom>
+          My Events
+        </Typography>
+      </Box>
+      <Grid className={classes.grid}>
+        {data &&
+          data?.events?.map((event, id) => (
+            <MyEventCard key={id} event={event} />
+          ))}
+      </Grid>
+    </main>
   );
 }
