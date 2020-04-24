15cf5d3b0542525ce18bb28aea9655ec471d8062
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-04-24 13:12:32 -0400

    Migrate from Apollo Boost to Apollo Client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 // Import dependencies
 import React from "react";
+
 // Reach Router imports
 import { Router, Link } from "@reach/router";
+
 // Import route components
 import DashRouter from "./routes/DashRouter";
 import PrivateRoute from "./routes/PrivateRoute";
+
 // Import page components
 import EventsCalendar from "./pages/EventsCalendar";
 import UserProfile from "./pages/UserProfile";
@@ -18,21 +21,19 @@ import ManageUsers from "./pages/ManageUsers";
 
 // Import apollo server
 import { ApolloProvider } from "@apollo/react-hooks";
-// import ApolloClient from "apollo-boost";
-
-// Google Analytics Imports
-import ReactGA from "react-ga";
-// Auth0 imports
-import { useAuth0 } from "./config/react-auth0-spa";
-import "./styles.css";
-
-// New Apollo Migration Imports
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
-// import { withClientState } from 'apollo-link-state';
-import { ApolloLink, Observable } from 'apollo-link';
+import { setContext } from "apollo-link-context";
+import { ApolloLink } from 'apollo-link';
+
+// Google Analytics Imports
+import ReactGA from "react-ga";
+
+// Auth0 imports
+import { useAuth0 } from "./config/react-auth0-spa";
+import "./styles.css";
 
 const trackingId = "UA-159556430-1";
 
@@ -44,41 +45,38 @@ const trackingId = "UA-159556430-1";
 function App() {
   const { getIdTokenClaims } = useAuth0();
 
-  // OLD Generate new apollo client start
-  // const client = new ApolloClient({
-  //   uri: process.env.REACT_APP_API_URL,
-  //   credentials: "same-origin",
-  //   request: async operation => {
-  //     const token = await getIdTokenClaims();
-      // Attach token to header
-  //     operation.setContext(context => ({
-  //       headers: {
-  //         ...context.headers,
-  //         Authorization: token.__raw,
-  //       },
-  //     }));
-  //   },
-  // });
-  // OLD Generate new apollo client end
+  const cache = new InMemoryCache();
 
-  //  New Generate new apollog client start
-  const cache = new InMemoryCache({}) 
-  const request = async (operation) => {
-  const token = await getIdTokenClaims();
-    operation.setContext({
-      headers: {
-        authorization: token.__raw,
-      }
-    });
-  };
+  const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+    if (graphQLErrors) {
+      graphQLErrors.forEach(({ message, path }) =>
+        console.log(`[GraphQL error]: Message: ${message}, Path: ${path}`)
+      );
+    }
   
+    if (networkError) {
+      console.log(
+        `[Network error ${operation.operationName}]: ${networkError.message}`
+      );
+    }
+  });
+  
+  const authLink = setContext((_, { headers }) => {
+    const context = {
+      headers: {
+        ...headers,
+        Authorization: `bearer ${sessionStorage.getItem("token")}`
+      }
+    };
+    return context;
+  });
+  
+  const httpLink = new HttpLink({ uri: process.env.REACT_APP_API_URL });
   
   const client = new ApolloClient({
-    link: new HttpLink({ uri: process.env.REACT_APP_API_URL }),
-    cache: new InMemoryCache()
+    link: ApolloLink.from([errorLink, authLink, httpLink]),
+    cache
   });
-
-
 
   return (
     <ApolloProvider client={client}>
