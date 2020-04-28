2a0ce9e3eb88085e173e22e0231a05565abfa0f0
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-04-28 13:54:28 -0400

    Add recent recipient query

--- src/pages/Chat/queries/ChatRooms.js
+++ src/pages/Chat/queries/ChatRooms.js
@@ -1,9 +1,9 @@
 import gql from "graphql-tag";
 
-// List all chat rooms for a user
+// List all chat rooms for a user and get list of all previous recipients
 export const GET_CHAT_ROOMS = gql`
-  query getChatRooms($email: String!) {
-    profile(where: {email: $email}) {
+  query getChatRooms( $email: String! ) {
+    profile( where: { email: $email } ) {
       chatRooms {
         id
         participants {
@@ -17,8 +17,8 @@ export const GET_CHAT_ROOMS = gql`
 
 // Retrieve messages for a user's chat room
 export const GET_CHAT_ROOM_MESSAGES = gql`
-  query getChatRoomMessages($email: String!) {
-    profile(where: {email: $email}){
+  query getChatRoomMessages( $email: String! ) {
+    profile( where: { email: $email } ){
       chatRooms {
         id
         chats {
@@ -35,29 +35,29 @@ export const GET_CHAT_ROOM_MESSAGES = gql`
   }
 `
 
-// Delete a chat room
-export const DELETE_CHAT_ROOM = gql`
-  mutation deleteChatRoom() {
-    deleteChatRoom() {
-      
-    }
-  }
-`;
+// // Delete a chat room
+// export const DELETE_CHAT_ROOM = gql`
+//   mutation deleteChatRoom() {
+//     deleteChatRoom() {
 
-// Add users to a chat room
-export const ADD_CHAT_ROOM_PARTICIPANTS = gql`
-  mutation addChatRoomParticipants() {
-    updateChatRoom() {
+//     }
+//   }
+// `;
 
-    }
-  }
-`;
+// // Add users to a chat room
+// export const ADD_CHAT_ROOM_PARTICIPANTS = gql`
+//   mutation addChatRoomParticipants() {
+//     updateChatRoom() {
 
-// Delete users from a chat room
-export const DELETE_CHAT_ROOM_PARTICIPANTS = gql`
-  mutation deleteChatRoomParticipants() {
-    updatedChatRoom() {
+//     }
+//   }
+// `;
 
-    }
-  }
-`;
+// // Delete users from a chat room
+// export const DELETE_CHAT_ROOM_PARTICIPANTS = gql`
+//   mutation deleteChatRoomParticipants() {
+//     updatedChatRoom() {
+
+//     }
+//   }
+// `;
