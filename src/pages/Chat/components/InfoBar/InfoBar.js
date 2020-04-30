44858fc90379b0517347f3de9fda6852c67f8276
Priya <pvaidya56@gmail.com>
2020-04-30 14:05:35 -0700

    Created basic modal for New Message

--- src/pages/Chat/components/InfoBar/InfoBar.js
+++ src/pages/Chat/components/InfoBar/InfoBar.js
@@ -13,19 +13,12 @@ import {
     Toolbar,
     Button,
   } from "@material-ui/core";
-  import MenuIcon from "@material-ui/icons/Menu";
-  import CircularProgress from "@material-ui/core/CircularProgress";
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import MenuIcon from "@material-ui/icons/Menu";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-
-// const MyButton = styled(Button)({
-//     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-//     border: 0,
-//     borderRadius: 3,
-//     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-//     color: 'white',
-//     height: 48,
-//     padding: '0 30px',
-//   });
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -33,10 +26,22 @@ const useStyles = makeStyles(theme => ({
       flexDirection: "column",
       border: "none",
     },
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
 }));
 
 function InfoBar({ user }) {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
     const { loading, error, data, refetch } = useQuery(GET_CHAT_ROOMS, { variables: { email: user.email } });
     
     // refetches CHAT_ROOMS without refreshing page
@@ -46,11 +51,38 @@ function InfoBar({ user }) {
 
     if (loading) return <CircularProgress className={classes.loadingSpinner} />;
     if (error) return `Error! ${error.message}`;
+
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
   
     return (
       <div>
           <h1>Messages</h1>
-          <Button>New Message</Button>
+          <Button type="button" onClick={handleOpen}>New Message</Button>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            className={classes.modal}
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <div className={classes.paper}>
+                <h2 id="transition-modal-title">Transition modal</h2>
+                <p id="transition-modal-description">react-transition-group animates me.</p>
+              </div>
+            </Fade>
+           </Modal> 
           {/* {user && user[config.roleUrl].includes("Admin") ? (
               <> */}
           <Button>New Annoucement</Button>
