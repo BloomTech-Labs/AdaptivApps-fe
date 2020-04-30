2b4719093a537eaeb22c6feb40ccee357fd9fec4
Priya <pvaidya56@gmail.com>
2020-04-30 16:45:25 -0700

    Added styling to modal, restricted new announcements option to admins only

--- src/pages/Chat/components/InfoBar/InfoBar.js
+++ src/pages/Chat/components/InfoBar/InfoBar.js
@@ -3,13 +3,21 @@ import { styled } from '@material-ui/core/styles';
 import { useQuery } from "react-apollo";
 import { GET_CHAT_ROOMS } from '../../queries/ChatRooms';
 import ChatRoom from './ChatRoom';
+
+//Auth0 imports
+import config from "../../../../config/auth_config";
+
 // Style Imports
 import CreateIcon from '@material-ui/icons/Create';
 import LanguageIcon from '@material-ui/icons/Language';
 import {
     Container,
     makeStyles,
-    Button
+    Button,
+    Icon,
+    Box,
+    TextField
+
   } from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Modal from '@material-ui/core/Modal';
@@ -30,17 +38,21 @@ const useStyles = makeStyles(theme => ({
     fontFamily: 'Arial',
   },
   icons: {
-    fontSize: '5rem',
-    color: 'grey'
+    fontSize: '4rem',
+    color: 'grey',
+    padding: "0%",
+    cursor: "pointer",
   },
   span: {
     fontSize: '2rem',
-    color: 'grey'
+    color: 'grey',
+    cursor: 'pointer'
   },
     modal: {
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
+      fontSize: "-webkit-xxx-large",
     },
     paper: {
       backgroundColor: theme.palette.background.paper,
@@ -50,10 +62,25 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-function InfoBar({ user }) {
+function InfoBar(props) {
+    const { user } = props;
     const classes = useStyles();
     const [open, setOpen] = React.useState(false);
+    const [searchRecipient, setSearchRecipient] = React.useState("");
+    const [searchResults, setSearchResults] = React.useState([]);
     const { loading, error, data, refetch } = useQuery(GET_CHAT_ROOMS, { variables: { email: user.email } });
+    
+    const handleChange = e => {
+      setSearchRecipient(e.target.value);
+    };
+
+    // useEffect(() => {
+    //   const results = people.filter(person =>
+    //     person.toLowerCase().includes(searchRecipient)
+    //   );
+    //   setSearchResults(results);
+    // }, [searchRecipient]);
+
     
     // refetches CHAT_ROOMS without refreshing page
     useEffect(() => {
@@ -78,9 +105,9 @@ function InfoBar({ user }) {
   
     return (
       <div>
-        <div className={classes.icons}></div>
+        {/* <div className={classes.icons}></div> */}
           <h1 className={classes.header}>Messages</h1>
-          <CreateIcon onClick={handleOpen} /><span className={classes.span}>New Message</span>
+          <CreateIcon className={classes.icons} onClick={handleOpen} /><span className={classes.span} onClick={handleOpen}>New Message</span>
           <Modal
             aria-labelledby="transition-modal-title"
             aria-describedby="transition-modal-description"
@@ -95,18 +122,43 @@ function InfoBar({ user }) {
           >
             <Fade in={open}>
               <div className={classes.paper}>
-                <h2 id="transition-modal-title">Select a Recipient</h2>
-                <p id="transition-modal-description">react-transition-group animates me.</p>
+                <h2 id="transition-modal-title" className={classes.span}>Select a Recipient</h2>
+                  {/* Search for Recipients functionality */}
+                  <div>       
+  
+                   <Box component="div">
+                    <TextField
+                      variant="outlined"
+                      type="text"
+                      placeholder="Search for a Recipient"
+                      name="message"
+                      value={searchRecipient}
+                      onChange={handleChange}
+                      />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        endIcon={<Icon>add_circle</Icon>}
+                    >
+                    Select
+                </Button>
+                </Box>
+                    {/* <ul>
+                      {searchResults.map(item => (
+                        <li>{item}</li>
+                      ))}
+                    </ul> */}
+                  </div>
               </div>
             </Fade>
            </Modal> 
-          {/* {user && user[config.roleUrl].includes("Admin") ? (
-              <> */}
-          <div className={classes.icons}>
-              <LanguageIcon fontSize='5rem' onClick={newAnnouncementClick}/><span className={classes.span}>New Announcement</span> 
+          {user && user[config.roleUrl].includes("Admin") ? (
+              <>
+          <div>
+              <LanguageIcon className={classes.icons} onClick={newAnnouncementClick}/><span className={classes.span} onClick={newAnnouncementClick}>New Announcement</span> 
             </div>
-          {/* </>
-          ): null} */}
+          </>
+          ): null}
           <div>
           {data &&
           data?.profile.chatRooms?.map((chatRoom, id) => (
