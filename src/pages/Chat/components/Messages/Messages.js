61dff9c4d390fe0fbc16ae9def0d19b82633e689
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-05-01 13:00:20 -0400

    updated to function properly

--- src/pages/Chat/components/Messages/Messages.js
+++ src/pages/Chat/components/Messages/Messages.js
@@ -1,48 +1,32 @@
-import React, { useEffect } from 'react'
-import { useQuery } from "react-apollo";
-import { GET_CHAT_ROOM_MESSAGES } from '../../queries/ChatRooms';
+import React from 'react';
+import TextContainer from "../TextContainer/TextContainer";
 import {
-    makeStyles,
-    useTheme,
-    Box,
-    Drawer,
-    Hidden,
-    IconButton,
-    Toolbar,
-    Button,
+    makeStyles
   } from "@material-ui/core";
-  import MenuIcon from "@material-ui/icons/Menu";
-  import CircularProgress from "@material-ui/core/CircularProgress";
 
-  const useStyles = makeStyles(theme => ({
-    root: {
-      display: "flex",
-      flexDirection: "column",
-      border: "none",
-    },
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    border: "none",
+  }
 }));
 
 export default function Messages({ user, chatRoom }) {
     const classes = useStyles();
-    //const { loading, error, data, refetch } = useQuery(GET_CHAT_ROOM_MESSAGES, { variables: { email: user.email } });
-
-    // refetches CHAT_ROOM_MESSAGES without refreshing page
-    // useEffect(() => {
-    //     refetch();
-    // }, [refetch]);
 
     const messages = chatRoom.chats.map((chat, id) => {return [
+        id,
         chat.message,
         chat.createdAt,
-        
-    ]})
-
-    // if (loading) return <CircularProgress className={classes.loadingSpinner} />;
-    // if (error) return `Error! ${error.message}`;
+        chat.from.firstName,
+        chat.from.lastName
+    ]});
 
     return (
         <div>
            {[messages]}
+           <TextContainer />
         </div>
     )
 }
--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -1,27 +1,32 @@
 // React imports
 import React from "react";
-import TextContainer from "./components/TextContainer/TextContainer";
+import InfoBar from './components/InfoBar/InfoBar';
+import {
+  makeStyles
+} from "@material-ui/core";
+
 // Auth0 imports
 import { useAuth0 } from "../../config/react-auth0-spa";
 
-// import NavBar from "./NavBar";
-import InfoBar from './components/InfoBar/InfoBar';
-import {
-  makeStyles,
-  Container,
-  Box,
-  Button,
-  Typography,
-} from "@material-ui/core";
-import ReactDOM from 'react-dom';
+const useStyles = makeStyles(() => ({
+  root: {
+    width: "25rem",
+    height: "100vh",
+    padding: "2%",
+    marginLeft: "-2rem",
+    border: "none",
+    boxShadow:
+      "0px 1px 3px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.12), 0px 0px 2px rgba(0, 0, 0, 0.14)",
+  }
+}))
 
 function ChatFeature(){
   const { user } = useAuth0();
+  const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
       <InfoBar user={user} />
-      <TextContainer />
     </div>
   )
 }
