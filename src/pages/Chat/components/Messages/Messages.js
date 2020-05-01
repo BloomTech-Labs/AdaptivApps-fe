ecdc11b136f14f4e68ad055899254e6d78751763
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-05-01 13:59:48 -0400

    Style messages

--- src/pages/Chat/components/Messages/Messages.js
+++ src/pages/Chat/components/Messages/Messages.js
@@ -9,23 +9,39 @@ const useStyles = makeStyles(theme => ({
     display: "flex",
     flexDirection: "column",
     border: "none",
+  },
+  messageText: {
+    marginTop: "0",
+    padding: "0 2%"
+  },
+  messageSender: {
+    marginLeft: "2%",
+    color: "#2962FF"
   }
 }));
 
 export default function Messages({ user, chatRoom }) {
     const classes = useStyles();
 
-    const messages = chatRoom.chats.map((chat, id) => {return [
-        id,
-        chat.message,
-        chat.createdAt,
-        chat.from.firstName,
-        chat.from.lastName
-    ]});
+    const messages = chatRoom.chats.map((chat, id) => {return {
+        id: id,
+        message: chat.message,
+        createdAt: chat.createdAt,
+        firstName: chat.from.firstName,
+        lastName: chat.from.lastName
+      }
+    });
 
     return (
         <div>
-           {[messages]}
+           {messages.map((message) => (
+             <>
+              <div className={classes.messageDiv}>
+                <span className={classes.messageSender}>{message.firstName} {message.lastName}</span>
+                <p className={classes.messageText}>{message.message}</p>
+              </div>
+             </>
+           ))}
            <Input />
         </div>
     )
