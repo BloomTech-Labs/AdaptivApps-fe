13cbb77490e35aa2cd084ae7696fc6df008df1ed
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-05-05 15:52:10 -0400

    Fix spacing on components

--- src/pages/Chat/components/InfoBar/Modal.js
+++ src/pages/Chat/components/InfoBar/Modal.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-apollo";
 import { GET_RECIPIENTS } from '../../queries/Chats';
 
@@ -9,11 +9,7 @@ import {
     TextField,
     MenuItem
   } from "@material-ui/core";
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import { FixedSizeList } from 'react-window';
-
-
 
 const useStyles = makeStyles(theme => ({
   span: {
@@ -21,75 +17,58 @@ const useStyles = makeStyles(theme => ({
     color: 'grey',
     cursor: 'pointer'
   },
-    modal: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontSize: "-webkit-xxx-large",
-    },
-    paper: {
-      backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-    },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: "-webkit-xxx-large",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
 }));
 
 function RecipientModal() {
-    const classes = useStyles();
-    const [searchRecipient, setSearchRecipient] = useState("");
-    const [searchResults, setSearchResults] = useState([]);
-    const { data } = useQuery(GET_RECIPIENTS);
-    
-    const handleChange = e => {
-      setSearchRecipient(e.target.value);
-    };
-
-
-    // useEffect(() => {
-    //   const results = searchResults.filter(person =>
-    //     person.toLowerCase().includes(searchRecipient)
-    //   );
-      
-    //   setSearchResults(searchResults);
-    // },[searchRecipient]);
-
-    return (
-      <div>          
-              <div className={classes.paper}>
-                <h2 id="transition-modal-title" className={classes.span}>Select a Recipient</h2>
-                  {/* Search for Recipients functionality */}
-                  <div>       
+  const classes = useStyles();
+  const [searchRecipient, setSearchRecipient] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const { data } = useQuery(GET_RECIPIENTS);
   
-                   <Box component="div">
-                    <TextField
-                      variant="outlined"
-                      type="text"
-                      placeholder="Search for a Recipient"
-                      name="message"
-                      value={searchRecipient}
-                      onChange={handleChange}
-                      />
-                            {/* {data && data?.profiles.map(item => (
-                                <MenuItem 
-                                value={`${item.firstName} ${item.lastName}`}
-                            >{`${item.firstName} ${item.lastName}`}</MenuItem>
-                                ))} */}
-                        <div className={classes.root}>
-                          <FixedSizeList height={400} width={300} itemSize={46} itemCount={200}>
-                          {data && data?.profiles.map(item => (
-                                <MenuItem 
-                                value={`${item.firstName} ${item.lastName}`}
-                            >{`${item.firstName} ${item.lastName}`}</MenuItem>
-                                ))}
-                          </FixedSizeList>
-                        </div>
-                </Box>
-                    
-                  </div>
-              </div>
+  const handleChange = e => {
+    setSearchRecipient(e.target.value);
+  };
+
+  return (
+    <div>          
+      <div className={classes.paper}>
+        <h2 id="transition-modal-title" className={classes.span}>Select a Recipient</h2>
+        {/* Search for Recipients functionality */}
+        <div>       
+          <Box component="div">
+            <TextField
+              variant="outlined"
+              type="text"
+              placeholder="Search for a Recipient"
+              name="message"
+              value={searchRecipient}
+              onChange={handleChange}
+              />
+            <div className={classes.root}>
+              <FixedSizeList height={400} width={300} itemSize={46} itemCount={200}>
+                {data && data?.profiles.map(item => (
+                  <MenuItem value={`${item.firstName} ${item.lastName}`}>
+                    {`${item.firstName} ${item.lastName}`}</MenuItem>
+                ))}
+              </FixedSizeList>
+            </div>
+          </Box>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default RecipientModal;
