c28ab413899afe3b618050bb909fb80004cd4573
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-05-08 17:12:21 -0400

    creates CreateActivity component

--- src/pages/CreateEvent/CreateActivity.js
+++ src/pages/CreateEvent/CreateActivity.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { makeStyles, Box, Typography } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { useMutation } from "react-apollo";
+import { CREATE_ACTIVITY } from "./graphql";
+
+import ActivityForm from "./ActivityForm";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: "100%",
+    width: "90%",
+    fontFamily: ["Roboto", "Helvetica", "Arial", "sans-serif"],
+  },
+  headingBox: {
+    margin: "6rem 0 2rem 3rem",
+    fontWeight: "400",
+    borderColor: "#D3D3D3",
+  },
+  loadingSpinner: {
+    position: "absolute",
+    top: "50%",
+    right: "50%",
+    color: "#2763FF",
+  },
+});
 
 export default function CreateActivity() {
+  const [createActivity, { data, error, loading }] = useMutation(CREATE_ACTIVITY);
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <main className={classes.root}>
+      <Box className={classes.headingBox} borderBottom={2}>
+        <Typography className={classes.heading} variant="h1" gutterBottom>
+          Add Activities to an Event
+        </Typography>
+      </Box>
+      <Box>
+        <ActivityForm createActivity={createActivity} />
+      </Box>
+    </main>
   );
 }
