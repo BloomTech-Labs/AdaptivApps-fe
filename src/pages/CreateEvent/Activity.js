ca074eb833e3cd14eb149b137a1f41a768f7c32f
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-05-08 19:22:27 -0400

    displays activity information on Activity component

--- src/pages/CreateEvent/Activity.js
+++ src/pages/CreateEvent/Activity.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function Activity() {
+export default function Activity({ activity }) {
   return (
     <div>
-      <h1>Hey Gurl!</h1>
+      <h1>{activity.name}</h1>
+      <h1>{activity.date}</h1>
+      <h1>{activity.location}</h1>
+      <h1>{activity.startTime} - {activity.endTime}</h1>
     </div>
   )
 }
