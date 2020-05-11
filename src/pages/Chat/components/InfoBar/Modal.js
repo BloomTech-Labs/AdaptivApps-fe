2b4f7e4af5066fe224a0be983032531830449178
Amber Mohler <ambermohler@Ambers-iMac.local>
2020-05-11 16:15:45 -0400

    using new message button will not display existing chatrooms as an option

--- src/pages/Chat/components/InfoBar/Modal.js
+++ src/pages/Chat/components/InfoBar/Modal.js
@@ -96,25 +96,19 @@ function RecipientModal({ user, setOpen, participants }) {
           return results;
         }
       });
-
       setSearchRecipient('');
     };
 
     const participantsEmail = participants.map(item => item.email)
     const uniqueEmails = [...new Set(participantsEmail)]
 
-    const newChatRoom = (item) => {
-      const filtered = uniqueEmails.filter(email => email === item.email)
-
-      filtered[0] !== item.email ? 
-      (createChatRoom({
+    const newChatRoom = async (item) => {
+        await (createChatRoom({
         variables:{
           useremail: user.email,
           recipientemail: item.email
         }
-      })) :
-       alert('You are chatting with this person already')
-      ;
+      }));
       setOpen(false);
     };
 
@@ -127,6 +121,33 @@ function RecipientModal({ user, setOpen, participants }) {
       e.preventDefault();
       setOpen(false);
     };
+
+    const searchList = (
+    <List>
+    {results.length > 0 ? 
+      (results.map(item => {
+        const filtered = uniqueEmails.filter(email => email === item.email)
+        if (filtered[0] !== item.email) {
+          return (
+            <ListItem className={classes.listItem} value={`${item.firstName} ${item.lastName}`} onClick={() => newChatRoom(item)}>
+              <ListItemText primary={`${item.firstName} ${item.lastName}`} />
+            </ListItem>
+          )
+        }
+        }))
+      : 
+      (data && data?.profiles.map(item => {
+        const filtered = uniqueEmails.filter(email => email === item.email)
+        if (filtered[0] !== item.email) {
+          return (
+            <ListItem className={classes.listItem} value={`${item.firstName} ${item.lastName}`} onClick={() => newChatRoom(item)}>
+                <ListItemText primary={`${item.firstName} ${item.lastName}`} />
+              </ListItem>
+          )
+        }
+        }))}
+      </List>
+      )
 
     return (
      <div>          
@@ -153,20 +174,7 @@ function RecipientModal({ user, setOpen, participants }) {
             <div className={classes.root}>
               <div>
               <Paper style={{maxHeight: 200, overflow: 'auto'}}>
-                <List>
-                {results.length > 0 ? 
-                  (results.map(item => item.email !== user.email && (
-                    <ListItem className={classes.listItem} value={`${item.firstName} ${item.lastName}`} onClick={() => newChatRoom(item)}>
-                      <ListItemText primary={`${item.firstName} ${item.lastName}`} />
-                    </ListItem>
-                  ))) 
-                  : 
-                  (data && data?.profiles.map(item => item.email !== user.email && (
-                    <ListItem className={classes.listItem} value={`${item.firstName} ${item.lastName}`} onClick={() => newChatRoom(item)}>
-                      <ListItemText primary={`${item.firstName} ${item.lastName}`} />
-                    </ListItem>
-                )))}
-                  </List>
+                {searchList}
                 </Paper>
               </div>
             </div>
