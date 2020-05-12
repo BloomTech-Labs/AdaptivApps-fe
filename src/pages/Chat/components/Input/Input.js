030a345b2b68e8bf310c2f07450dd987891b2d54
Priya <pvaidya56@gmail.com>
2020-05-12 15:39:29 -0700

    Speech to text functionality works!

--- src/pages/Chat/components/Input/Input.js
+++ src/pages/Chat/components/Input/Input.js
@@ -67,13 +67,16 @@ const Input = ({ chatRoom, user }) => {
     const classes = useStyles();
     const [toggleEmoji, setToggleEmoji] = useState(false);
 
+   
+
+    const onResult = result => {
+        setTextValue(result);
+    }
+    
     //Setting state for speech recognition
     const [textValue, setTextValue] = useState("");
-    const { listen, listening, stop } = useSpeechRecognition({
-        onResult: result => {
-          setTextValue(result);
-        }
-      });
+    
+    
     
     const [sendChat] = useMutation(SEND_CHAT);
     const [message, setMessage] = useState('');
@@ -87,6 +90,11 @@ const Input = ({ chatRoom, user }) => {
         setToggleEmoji(false)
     };
    
+    const onEnd =  () => {
+        console.log("Listening has finished")
+      };
+
+      const { listen, listening, stop } = useSpeechRecognition({onResult, onEnd});
     const newMessage = async () => {
         await sendChat({
             variables: {
@@ -103,17 +111,25 @@ const Input = ({ chatRoom, user }) => {
             variables: {
               id: chatRoom.id,
               email: user.email,
-              message: textValue.textValue 
+              message: textValue
             }
         })
-        setTextValue({ message: ''})
+        setTextValue('');
     };
 
    const handleChange = e => {
+       console.log(e.target, "event");
         setMessage({
             message: e.target.value
         })
     };
+
+    // const handleSpeechChange = (e, result) => {
+    //     console.log(e, "event");
+    //      setTextValue({
+    //          textValue: e.target.value
+    //      })
+    //  };
 
     const onEmojiClick = (e) => {
         setMessage({
@@ -132,20 +148,20 @@ const Input = ({ chatRoom, user }) => {
                     className={classes.messageBox}
                     multiline={true}
                     rowsMax='4'
-                    value={message.message}
-                    value={textValue}
+                    value={message.message || textValue}
                     variant="outlined"
                     type="text"
                     name="newChat"
                     placeholder="Type a message..."
-                    onChange={handleChange}
-                    onChange={event => setTextValue(event.target.value)}
+                    onChange={textValue ? onResult : handleChange }
                     InputProps={{
                         endAdornment: <InputAdornment position="end">
                         <SendIcon
                         className={classes.sendMessageIcon} 
-                        onClick={newMessage}
-                        onClick={newSpeechMessage} />
+                        onClick={textValue ? newSpeechMessage : newMessage} 
+                      
+                        
+                         />
                     </InputAdornment>
                     }}
                     />
@@ -164,3 +180,124 @@ const Input = ({ chatRoom, user }) => {
 }
 
 export default Input;
+
+// // React imports
+// import React, { useState } from "react";
+// // Query Imports
+// import { SEND_CHAT } from '../../queries/Chats'
+// import { useMutation } from 'react-apollo'
+// //Emoji Picker Import
+// import 'emoji-mart/css/emoji-mart.css'
+// import { Picker } from 'emoji-mart'
+// //Styling Imports
+// import InputAdornment from '@material-ui/core/InputAdornment';
+// import SendIcon from '@material-ui/icons/Send';
+// import MoodIcon from '@material-ui/icons/Mood';
+// import Modal from '@material-ui/core/Modal';
+// import {
+//     makeStyles,
+//     TextField
+//   } from "@material-ui/core";
+// const useStyles = makeStyles(() => ({
+//     inputDiv: {
+//         width: '95%',
+//         margin: 'auto',
+//         display: 'flex',
+//         alignItems: 'center'
+//     },
+//     iconDiv: {
+//         width: '15%',
+//         display: 'flex',
+//         justifyContent: 'space-evenly'
+//     },
+//     messageBox: {
+//         width: "80%",
+//         marginLeft: '3%'
+//     },
+//     icons: {
+//         color: '#808080',
+//         fontSize: '3.5rem',
+//         '&:hover': {
+//             cursor: "pointer",
+//           },
+//     },
+//     sendMessageIcon: {
+//         color: '#2962FF',
+//         fontSize: '3rem',
+//         '&:hover': {
+//             cursor: "pointer",
+//           },
+//     },
+//     modal: {
+//         display: 'flex',
+//         alignItems: 'center',
+//         justifyContent: 'flex-end',
+//         fontSize: "-webkit-xxx-large",
+//     }
+// }));
+// const Input = ({ chatRoom, user }) => {
+//     const classes = useStyles();
+//     const [toggleEmoji, setToggleEmoji] = useState(false)
+//     const [sendChat] = useMutation(SEND_CHAT);
+//     const [message, setMessage] = useState('');
+//     const emojiOpen = () => {
+//         setToggleEmoji(true)
+//     };
+//     const emojiClose = () => {
+//         setToggleEmoji(false)
+//     };
+//     const newMessage = async () => {
+//         await sendChat({
+//             variables: {
+//               id: chatRoom.id,
+//               email: user.email,
+//               message: message.message
+//             }
+//         })
+//         setMessage({ message: ''})
+//     };
+//    const handleChange = e => {
+//         setMessage({
+//             message: e.target.value
+//         })
+//     };
+//     const onEmojiClick = (e) => {
+//         setMessage({
+//             message: message.message ? message.message + e.native : e.native
+//         });
+//     };
+//     return(
+//         <div>
+//             <div className={classes.inputDiv}>
+//                 <TextField
+//                     className={classes.messageBox}
+//                     multiline={true}
+//                     rowsMax='4'
+//                     value={message.message}
+//                     variant="outlined"
+//                     type="text"
+//                     name="newChat"
+//                     placeholder="Type a message..."
+//                     onChange={handleChange}
+//                     InputProps={{
+//                         endAdornment: <InputAdornment position="end">
+//                         <SendIcon
+//                         className={classes.sendMessageIcon}
+//                         onClick={newMessage} />
+//                     </InputAdornment>
+//                     }}
+//                     />
+//                 <div className={classes.iconDiv}>
+//                     <MoodIcon className={classes.icons} onClick={emojiOpen}/>
+//                     <Modal
+//                         className={classes.modal}
+//                         open={toggleEmoji}
+//                         onClose={emojiClose}>
+//                         {toggleEmoji ? <Picker onClick={onEmojiClick}/> : null}
+//                     </Modal>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+// export default Input;
