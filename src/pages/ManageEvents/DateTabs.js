b31c156ca8ba6c0a0ab9071f904ac9b884f60f08
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-05-14 15:03:18 -0400

    creates onSubmit for groupBy function

--- src/pages/ManageEvents/DateTabs.js
+++ src/pages/ManageEvents/DateTabs.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
-import { Tabs, Tab, Typography, Box, AppBar } from "@material-ui/core";
+import { Tabs, Tab, Typography, Box, AppBar, Button } from "@material-ui/core";
 import moment from "moment";
 import ActivityList from "./ActivityList";
 
@@ -49,6 +49,8 @@ const useStyles = makeStyles(theme => ({
 export default function DateTabs({ data, refetch }) {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+  const [activityByDates, setActivityByDates] = useState();
+  const [activityGroup, setActivityGroup] = useState();
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -77,9 +79,38 @@ export default function DateTabs({ data, refetch }) {
   };
   const days = getDatesRangeArray(startDate, endDate);
 
-  const activityDate = data?.event?.activities.map(activity => {
-    return activity?.date;
+  // const activityDate = data?.event?.activities.map(activity => {
+  //   return activity?.date;
+  // });
+
+  useEffect(() => {
+    setActivityByDates(data?.event?.activities);
   });
+
+  console.log("Inside DateTabs", data?.event?.activities);
+
+  console.log(activityByDates);
+
+  const groupBy = (array, key) => {
+    // Return the end result
+    return array.reduce((result, currentValue) => {
+      // If an array already present for key, push it to the array. Else create an array and push the object
+      (result[currentValue[key]] = result[currentValue[key]] || []).push(
+        currentValue
+      );
+      // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
+      return result;
+    }, {}); // empty object is the initial value for result object
+  };
+
+  const onSubmit = async () => {
+    const activitiesGroupedByDate = await groupBy(activityByDates, "date");
+    console.log(activitiesGroupedByDate);
+    await setActivityGroup(activitiesGroupedByDate);
+   
+  };
+  console.log('inside submit', activityGroup);
+
   return (
     <div className={classes.root}>
       <AppBar position="static" color="default">
@@ -97,8 +128,9 @@ export default function DateTabs({ data, refetch }) {
           ))}
         </Tabs>
       </AppBar>
-      <TabPanel value={value} index={0}>
+      <TabPanel value={value} index={1}>
         <ActivityList data={data} refetch={refetch} />
+        <Button onClick={onSubmit}>Push Me</Button>
       </TabPanel>
     </div>
   );
