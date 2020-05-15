2d570c1114617b478a07ec071bc98bb33b722edb
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-05-15 19:36:01 -0400

    creates cta button and paragraph in AccountSettings.js

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -6,14 +6,37 @@ import { useQuery, useMutation } from "react-apollo";
 // import ProfileForm from "./ProfileForm";
 import { ADD_USER_PROFILE, PROFILE_INFO } from "./queries";
 // Material-UI imports
-import { makeStyles, Box, Typography } from '@material-ui/core';
+import { makeStyles, Container, Box, Typography, Button } from '@material-ui/core';
 
 const useStyles = makeStyles({
+  root: {
+
+  },
   headingBox: {
     margin: "6rem 0 2rem 3rem",
     fontWeight: "400",
     borderColor: "#D3D3D3"
   },
+  ctaBox: {
+
+  },
+  ctaBtn: {
+    textTransform: "none",
+    marginTop: "2.4rem",
+    background: "#2962FF",
+    color: "#FFFFFF",
+    width: 360,
+    height: 48,
+    "& .MuiButton-label": {
+      fontSize: "2.1rem",
+      fontWeight: 550,
+    },
+    "&:hover": {
+      border: "1px solid #2962FF",
+      background: "white",
+      color: "#2962FF",
+    },
+  }
 });
 
 export default function Settings() {
@@ -46,11 +69,26 @@ export default function Settings() {
   };
 
   return (
-    <Box className={classes.headingBox} borderBottom={2}>
+    <Container>
+      <Box className={classes.headingBox} borderBottom={2}>
       <Typography variant="h1" gutterBottom>
         Account Settings
       </Typography>
     </Box>
+    <Box className={classes.ctaBox}>
+      <Typography>
+        Help us bring you the best
+      </Typography>
+      <Typography>
+        Angel City Sports experience--
+      </Typography>
+      <Typography>
+        Tell us a bit more about yourself!
+      </Typography>
+      <Button className={classes.ctaBtn}>Add my info</Button>
+    </Box>
+    </Container>
+    
     // <ProfileForm
     //   loading={loading}
     //   profile={profile ? profile : null}
