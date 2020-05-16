c282ef6233f07bc7cbbc5a38bc5e0e86490d3959
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-05-16 11:56:15 -0400

    creates reusable next button

--- src/pages/UserSettings/AccountInfo.js
+++ src/pages/UserSettings/AccountInfo.js
@@ -1,7 +1,16 @@
 // React/Reach Router imports
-import React from 'react'
+import React from "react";
+import { useNavigate } from "@reach/router";
+// Component imports
+import NextButton from '../../theme/NextButton';
 // Material-UI imports
-import { makeStyles, Typography, Container, Box } from '@material-ui/core'
+import {
+  makeStyles,
+  Typography,
+  Container,
+  Box,
+  Button,
+} from "@material-ui/core";
 
 const useStyles = makeStyles({
   headingBox: {
@@ -9,16 +18,21 @@ const useStyles = makeStyles({
     fontWeight: "400",
     borderColor: "#D3D3D3",
   },
-})
+});
 
 export default function AccountInfo() {
   const classes = useStyles();
+  const navigate = useNavigate();
 
   return (
     <Container>
-      <Box  className={classes.headingBox} borderBottom={2}>
+      <Box className={classes.headingBox} borderBottom={2}>
         <Typography variant="h1">Update Account Information</Typography>
       </Box>
+      <NextButton
+        aria-label="Click here to complete step 1 of update account information."
+        onClick={() => navigate(`/step1`)}
+      />
     </Container>
-  )
+  );
 }
---
+++ src/theme/NextButton.js
@@ -0,0 +1,32 @@
+import React from 'react'
+// Material-UI imports
+import { Button, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  button: {
+    textTransform: "none",
+    marginTop: "2.4rem",
+    background: "#2962FF",
+    color: "#FFFFFF",
+    width: 96,
+    height: 48,
+    "& .MuiButton-label": {
+      fontSize: "2.1rem",
+      fontWeight: 500,
+    },
+    "&:hover": {
+      border: "1px solid #2962FF",
+      background: "white",
+      color: "#2962FF",
+    },
+  }
+})
+export default function NextButton(props) {
+  const classes = useStyles();
+
+  return (
+  <Button className={classes.button} onClick={props.onClick}>
+    Next
+  </Button>
+  )
+}
