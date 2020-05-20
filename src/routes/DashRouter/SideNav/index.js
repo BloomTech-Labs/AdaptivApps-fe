1794f14abd579264751346e8da24ec1720bc8e1f
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-05-20 12:21:42 -0400

    Setup profile subscription and add logic for null notifications while loading data

--- src/routes/DashRouter/SideNav/index.js
+++ src/routes/DashRouter/SideNav/index.js
@@ -158,7 +158,27 @@ function SideNav(props) {
   const { subscribeToMore: announcementSubscription, refetch: refetchAnnouncements  } = useQuery(GET_ANNOUNCEMENTS, { variables: { isAnnouncementRoom: true } });
   const { subscribeToMore } = useQuery(GET_MESSAGES, { variables: { email: user.email } });
   const { subscribeToMore: notificationSubscription } = useQuery(GET_NOTIFICATIONS, { variables: { email: user.email } });
-  const { data } = useQuery(GET_USER_PROFILE, { variables: { email: user.email } });
+  const { data, subscribeToMore: profileSubscription, refetch: refetchProfile } = useQuery(GET_USER_PROFILE, { variables: { email: user.email } });
+
+  // Profile Subscription
+  const _subscribeToProfileChanges = profileSubscription => {
+    profileSubscription({
+      document: GET_USER_PROFILE,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev
+        const profile = subscriptionData.data.profile
+        refetchProfile({ variables: { email: user.email } });
+        return Object.assign({}, prev, {
+          profile: {
+            profile, ...prev.profile,
+            __typename: prev.profile.__typename
+          }
+        })
+      }
+    })
+  };
+
+  _subscribeToProfileChanges(profileSubscription);
 
   // Chat Subscription
   const _subscribeToNewChats = subscribeToMore => {
@@ -180,7 +200,7 @@ function SideNav(props) {
 
   _subscribeToNewChats(subscribeToMore);
 
-  // Announcement Subscriptions
+  // Announcement Subscription
   const _subscribeToNewAnnouncements = announcementSubscription => {
     announcementSubscription({
       document: ANNOUNCEMENT_SUBSCRIPTION,
@@ -198,7 +218,7 @@ function SideNav(props) {
 
   _subscribeToNewAnnouncements(announcementSubscription);
 
-  // Notification Subscriptions
+  // Notification Subscription
   const _subscribeToNewNotifications = notificationSubscription => {
     notificationSubscription({
       document: NOTIFICATION_SUBSCRIPTION,
@@ -245,7 +265,7 @@ function SideNav(props) {
         (
           <Tooltip title="Please complete your profile information to access Chats">
             <div className={classes.disabledNavLink}>
-              {data && data?.profile.notifications.length > 0 ? (
+              {(data && data.profile.notifications !== null) && (data && data?.profile.notifications.length > 0) ? (
                 <Badge
                 variant='dot'
                 color='error' 
