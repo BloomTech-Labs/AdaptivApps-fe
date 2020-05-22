71b7da0bce19f1438e37482abe4422a30a39899e
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-05-22 13:11:39 -0400

    adds FinishButton to step6.js

--- src/pages/UserSettings/AccountInfo/Step6.js
+++ src/pages/UserSettings/AccountInfo/Step6.js
@@ -1,9 +1,28 @@
-import React from 'react'
+// React/Reach Router imports
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { useParams, useNavigate } from "@reach/router";
+// Component imports
+import FinishButton from "../../../theme/FormButton";
+// Material-UI imports
+import {
+  makeStyles,
+  Box,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
 
-export default function Step6() {
+export default function Step6({ updateDemo3 }) {
   return (
-    <div>
+    <form className={classes.root} onSubmit={handleSubmit(onSubmit)}>
       <h1>Step 6!!!!!</h1>
-    </div>
-  )
+      <FinishButton
+        type="submit"
+        label="Finish"
+        onClick={handleSubmit}
+        ariaLabel="Click here to complete step 6 of account update and go back to account settings."
+      />
+    </form>
+  );
 }
