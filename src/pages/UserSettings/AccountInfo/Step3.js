29fde41ba033f05d3434247ca8ae11dd8edb538d
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-05-21 20:43:29 -0400

    completes functionality to update demo profile in step 3 of account update forms

--- src/pages/UserSettings/AccountInfo/Step3.js
+++ src/pages/UserSettings/AccountInfo/Step3.js
@@ -1,6 +1,6 @@
 // React/Reach Router imports
 import React from "react";
-import { useForm, Controller } from "reach-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { useParams, useNavigate } from "@reach/router";
 // Component imports
 import NextButton from "../../../theme/FormButton";
@@ -21,21 +21,17 @@ const useStyles = makeStyles({
     "& .MuiTextField-root": {
       width: 744,
       height: 48,
-      marginBottom: "2.4rem",
     },
   },
-  shortSelect: {
-    width: 360,
-    marginRight: "2.4rem",
-    marginBottom: "2.4rem",
+  spacing: {
+    marginTop: "2.4rem",
   },
-  longSelect: {
-    marginBottom: "2.4rem",
+  textBox: {
+    marginBottom: 240
   },
   btnBox: {
     display: "flex",
     justifyContent: "flex-end",
-    marginTop: "11.2rem",
   },
 });
 
@@ -60,14 +56,78 @@ export default function Step3({ updateDemoProfile }) {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h1>Step 3!!!!!</h1>
-      <NextButton
-        label="Next"
-        type="submit"
-        onClick={handleSubmit}
-        ariaLabel="Click here to complete step 3 and move onto step 4 of account information update."
+    <form className={classes.root} onSubmit={handleSubmit(onSubmit)}>
+      <InputLabel htmlFor="adaptivSportsParticipation">
+        Have you ever participated in adaptive sports before?
+      </InputLabel>
+      <Controller
+        as={
+          <Select>
+            <MenuItem value="Yes">Yes</MenuItem>
+            <MenuItem value="No">No</MenuItem>
+          </Select>
+        }
+        name="adaptivSportsParticipation"
+        type="select"
+        variant="outlined"
+        control={control}
+        defaultValue=""
       />
+      <InputLabel className={classes.spacing} htmlFor="acsParticipation">
+        Have you participated in Angel City Clinics before?
+      </InputLabel>
+      <Controller
+        as={
+          <Select>
+            <MenuItem value="Yes">Yes</MenuItem>
+            <MenuItem value="No">No</MenuItem>
+          </Select>
+        }
+        name="acsParticipation"
+        type="select"
+        variant="outlined"
+        control={control}
+        defaultValue=""
+      />
+      <InputLabel className={classes.spacing} htmlFor="angelCityParticipation">
+        Please select all Angel City Games you have participated in?
+      </InputLabel>
+      <Controller
+        as={
+          <Select>
+            <MenuItem value="Game 1">Game 1</MenuItem>
+            <MenuItem value="Game 2">Game 2</MenuItem>
+            <MenuItem value="Game 3">Game 3</MenuItem>
+          </Select>
+        }
+        name="angelCityParticipation"
+        type="select"
+        variant="outlined"
+        control={control}
+        defaultValue=""
+      />
+      <InputLabel className={classes.spacing} htmlFor="notParticipating">
+        If you are not participating in as many adaptive sports as you'd like,
+        can you please share why?
+      </InputLabel>
+      <Controller
+        as={<TextField />}
+        name="notParticipating"
+        variant="outlined"
+        control={control}
+        className={classes.textBox}
+        multiline
+        rows="8"
+        defaultValue=""
+      />
+      <Box className={classes.btnBox}>
+        <NextButton
+          label="Next"
+          type="submit"
+          onClick={handleSubmit}
+          ariaLabel="Click here to complete step 3 and move onto step 4 of account information update."
+        />
+      </Box>
     </form>
   );
 }
