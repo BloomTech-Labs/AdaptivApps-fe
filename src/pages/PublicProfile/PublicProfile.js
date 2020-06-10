f8c36dc4a1d32bf913d1e43160c3c460037140b7
Robert Campbell <robertc1191@Roberts-MacBook-Pro-2.local>
2020-06-10 15:54:27 -0700

    public profile with useParams

--- src/pages/PublicProfile/PublicProfile.js
+++ src/pages/PublicProfile/PublicProfile.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { useParams } from "@reach/router";
 import { useQuery } from "react-apollo";
 import { GET_USER_PROFILE } from "../PublicProfile/graphql";
 
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 export default function PublicProfile() {
+  const { userName } = useParams();
   const { data, loading, error } = useQuery(GET_USER_PROFILE, {
-    variables: { userName: data?.profile?.userName },
+    variables: { userName: userName },
   });
   if (loading) return <CircularProgress />;
   if (error) return `Error! ${error.message}`;
