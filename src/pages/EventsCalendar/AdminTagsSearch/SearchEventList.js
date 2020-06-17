e51c694c0014a9052f9e814719d916191b5bce4f
JonathanChen <jonny29009@yahoo.com>
2020-06-17 12:00:53 -0700

    fix filter bugs for event tag filtering

--- src/pages/EventsCalendar/AdminTagsSearch/SearchEventList.js
+++ src/pages/EventsCalendar/AdminTagsSearch/SearchEventList.js
@@ -5,32 +5,32 @@ import { Typography, makeStyles } from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 const useStyles = makeStyles(theme => ({
-    eventGroup: {
-        display: "flex",
-        flexDirection: "column",
-        flexWrap: "wrap",
-        "& h1": {
-            fontWeight: "600",
-            color: "#808080",
-        },
+  eventGroup: {
+    display: "flex",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    "& h1": {
+      fontWeight: "600",
+      color: "#808080",
     },
-    eventCard: {
-        display: "flex",
-        flexWrap: "wrap",
-    },
+  },
+  eventCard: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
 }));
 
 export default function SearchEventList({ eventsList, refetch }) {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.eventGroup}>
-            <Typography variant="h1">Events</Typography>
-            <div className={classes.eventCard}>
-                {eventsList?.map((event, id) => (
-                    <SearchEventCard key={id} event={event} refetch={refetch} />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className={classes.eventGroup}>
+      <Typography variant="h1">Events</Typography>
+      <div className={classes.eventCard}>
+        {eventsList?.map((event, id) => (
+          <SearchEventCard key={id} event={event} refetch={refetch} />
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/pages/EventsCalendar/AdminTagsSearch/index.js
+++ src/pages/EventsCalendar/AdminTagsSearch/index.js
@@ -13,120 +13,121 @@ import { makeStyles } from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 const useStyles = makeStyles({
-    wrapper: {
-        position: "relative",
-        width: "250px",
-        userSelect: "none",
-    },
-    header: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        position: "relative",
-        border: "1px solid rgb(223, 223, 223)",
-        borderRadius: "3px",
-        backgroundColor: "white",
-        lineHeight: "38px",
-        cursor: "default",
-        width: "100%",
-        fontSize: "2.4rem"
-    },
-    title: {
-        margin: "2px 20px",
-        marginRight: "30px",
-        fontWeight: 300
-    },
-    icons: {
-        marginRight: "20px",
-        color: "black"
-    },
-    loadingSpinner: {
-        position: "absolute",
-        top: "50%",
-        right: "50%",
-        color: "#2763FF",
-    },
+  wrapper: {
+    position: "relative",
+    width: "250px",
+    userSelect: "none",
+  },
+  header: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    position: "relative",
+    border: "1px solid rgb(223, 223, 223)",
+    borderRadius: "3px",
+    backgroundColor: "white",
+    lineHeight: "38px",
+    cursor: "default",
+    width: "100%",
+    fontSize: "2.4rem"
+  },
+  title: {
+    margin: "2px 20px",
+    marginRight: "30px",
+    fontWeight: 300
+  },
+  icons: {
+    marginRight: "20px",
+    color: "black"
+  },
+  loadingSpinner: {
+    position: "absolute",
+    top: "50%",
+    right: "50%",
+    color: "#2763FF",
+  },
 });
 
 const AdminTagsSearch = props => {
-    const classes = useStyles();
-    const [listOpen, setListOpen] = useState(false);
-    const [numSelected, setNumSelected] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const [selectedTags, setSelectedTags] = useState([]);
-    const { isSearching, setIsSearching } = props;
-    const { data, loading, error } = useQuery(GET_TAGS);
-    const { data: eventsData, loading: eventsLoading, error: eventsError, refetch } = useQuery(GET_EVENT_LIST);
+  const classes = useStyles();
+  const [listOpen, setListOpen] = useState(false);
+  const [numSelected, setNumSelected] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const [selectedTags, setSelectedTags] = useState([]);
+  const { isSearching, setIsSearching } = props;
+  const { data, loading, error } = useQuery(GET_TAGS);
+  const { data: eventsData, loading: eventsLoading, error: eventsError, refetch } = useQuery(GET_EVENT_LIST);
 
-    if (loading) return <CircularProgress className={classes.loadingSpinner} />;
-    if (error) return <p>{error}</p>;
-    if (eventsLoading) return <CircularProgress className={classes.loadingSpinner} />;
-    if (eventsError) return <p>{eventsError}</p>;
+  if (loading) return <CircularProgress className={classes.loadingSpinner} />;
+  if (error) return <p>{error}</p>;
+  if (eventsLoading) return <CircularProgress className={classes.loadingSpinner} />;
+  if (eventsError) return <p>{eventsError}</p>;
 
-    const toggleList = () => {
-        setListOpen(!listOpen);
-        setIsSearching(!isSearching);
-        setKeyword("")
+  const toggleList = () => {
+    setListOpen(!listOpen);
+    setIsSearching(!isSearching);
+    setKeyword("")
+  }
+
+  let eventsList = eventsData?.events;
+
+  const checkEventContainAllTags = (event, tags) => {
+    const eventTags = event.tags.split(", ");
+    for (let i = 0; i < tags.length; i++) {
+      if (!eventTags.includes(tags[i])) {
+        return false;
+      }
     }
+    return true;
+  }
 
-    let eventsList = eventsData?.events;
-
-    const filterEvents = events => {
-        let tempEventsList = [];
-        let hasAdded = false;
-        for (let i = 0; i < events.length; i++) {
-            const tags = events[i].tags.split(", ");
-            for (let j = 0; j < selectedTags.length; j++) {
-                if (!tags.includes(selectedTags[j])) {
-                    break;
-                }
-                if (!hasAdded) {
-                    tempEventsList.push(events[i]);
-                    hasAdded = true;
-                }
-            }
-            hasAdded = false;
-        }
-        return tempEventsList;
+  const filterEvents = events => {
+    let tempEventsList = [];
+    for (let i = 0; i < events.length; i++) {
+      if (checkEventContainAllTags(events[i], selectedTags)) {
+        tempEventsList.push(events[i]);
+      }
     }
+    return tempEventsList;
+  }
 
-    if (eventsList && selectedTags.length > 0) {
-        eventsList = filterEvents(eventsList);
-    }
+  if (eventsList && selectedTags.length > 0) {
+    eventsList = filterEvents(eventsList);
+  }
 
-    return (
-        <div>
-            <div className={classes.wrapper}>
-                <button type="button" className={classes.header} onClick={() => toggleList()}>
-                    {!isSearching
-                        ? <div className={classes.title}>Select Tags</div>
-                        : <div className={classes.title}>{numSelected} Tags Selected</div>
-                    }
-                    {listOpen
-                        ? <FontAwesomeIcon icon={faAngleUp} className={classes.icons} />
-                        : <FontAwesomeIcon icon={faAngleDown} className={classes.icons} />}
-                </button>
-                {listOpen && (
-                    <Dropdown
-                        tagsData={data.tags}
-                        keyword={keyword}
-                        setKeyword={setKeyword}
-                        setIsSearching={setIsSearching}
-                        setNumSelected={setNumSelected}
-                        selectedTags={selectedTags}
-                        setSelectedTags={setSelectedTags}
-                    />
-                )}
-            </div>
-            {isSearching
-                ?
-                <SearchEventList
-                    eventsList={eventsList}
-                    refetch={refetch}
-                />
-                : null}
-        </div>
-    )
+  return (
+    <div>
+      <div className={classes.wrapper}>
+        <button type="button" className={classes.header} onClick={() => toggleList()}>
+          {!isSearching
+            ? <div className={classes.title}>Select Tags</div>
+            : <div className={classes.title}>{numSelected} Tags Selected</div>
+          }
+          {listOpen
+            ? <FontAwesomeIcon icon={faAngleUp} className={classes.icons} />
+            : <FontAwesomeIcon icon={faAngleDown} className={classes.icons} />}
+        </button>
+        {listOpen && (
+          <Dropdown
+            tagsData={data.tags}
+            keyword={keyword}
+            setKeyword={setKeyword}
+            setIsSearching={setIsSearching}
+            setNumSelected={setNumSelected}
+            selectedTags={selectedTags}
+            setSelectedTags={setSelectedTags}
+          />
+        )}
+      </div>
+      {isSearching
+        ?
+        <SearchEventList
+          eventsList={eventsList}
+          refetch={refetch}
+        />
+        : null}
+    </div>
+  )
 }
 
 export default AdminTagsSearch;
