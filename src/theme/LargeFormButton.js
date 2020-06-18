df666b3f60e3df5b04358382c74c46fc7e8fbf8d
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-06-18 18:51:23 -0400

    creates reusable LargeFormButton component

---
+++ src/theme/LargeFormButton.js
@@ -0,0 +1,32 @@
+import React from 'react'
+// Material-UI imports
+import { Button, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  button: {
+    textTransform: "none",
+    // marginTop: "2.4rem",
+    background: "#2962FF",
+    color: "#FFFFFF",
+    width: 144,
+    height: 48,
+    "& .MuiButton-label": {
+      fontSize: "2.1rem",
+      fontWeight: 500,
+    },
+    "&:hover": {
+      border: "1px solid #2962FF",
+      background: "white",
+      color: "#2962FF",
+    },
+  }
+})
+export default function LargeFormButton({type, ariaLabel, label, onClick}) {
+  const classes = useStyles();
+
+  return (
+  <Button className={classes.button} type={type} aria-label={ariaLabel} onClick={onClick} >
+   {label}
+  </Button>
+  )
+}
