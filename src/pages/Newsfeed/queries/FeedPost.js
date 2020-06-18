02a2894953102ade26c0b0628d325faf653886a3
Amber Mohler <ambermohler@ambers-imac.lan>
2020-06-18 17:46:47 -0400

    adds feedpost queries, mutations, & subscription

---
+++ src/pages/Newsfeed/queries/FeedPost.js
@@ -0,0 +1,155 @@
+import gql from "graphql-tag";
+
+// Get Newsfeed Posts
+export const GET_NEWSFEED_POSTS = gql`
+  query {
+    feedPosts {
+      id
+      body
+      imgUrl
+      likes {
+        id
+        likedBy {
+          id
+          email
+        }
+        post {
+          id
+        }
+      }
+      postedBy {
+        id
+        email
+      }
+      createdAt
+    }
+  }
+`
+
+// Create a Newsfeed Post w/ no image
+export const CREATE_NEWSFEED_POST_NO_IMAGE = gql`
+  mutation createFeedPost($body: String!, $postedBy: String!) {
+    createFeedPost(
+      data: {
+        body: $body
+        postedBy: {
+          connect: {
+            email: $postedBy
+          }}
+      }) {
+        id
+        body
+        createdAt
+        postedBy {
+          id
+          email
+      }
+    }
+  }
+`
+
+// Create a Newsfeed Post w/ image
+export const CREATE_NEWSFEED_POST_WITH_IMAGE = gql`
+  mutation createFeedPost($body: String!, $imgUrl: String!, $postedBy: String!) {
+    createFeedPost(
+      data: {
+        body: $body
+        imgUrl: $imgUrl
+        postedBy: {
+          connect: {
+            email: $postedBy
+          }}
+      }) {
+        id
+        body
+        createdAt
+        postedBy {
+          id
+          email
+      }
+    }
+  }
+`
+
+// Update Newsfeed Post w/ no image
+export const UPDATE_NEWSFEED_POST_NO_IMAGE = gql `
+  mutation updateFeedPost($body: String!, $id: ID!) {
+    updateFeedPost(
+      data: {
+        body: $body
+        }
+        where: {
+          id: $id
+          }) {
+            id
+            body
+            imgUrl
+            postedBy {
+              id
+              email
+            }
+            updatedAt
+          }
+        }
+`
+
+// Update Newsfeed Post w/ image
+export const UPDATE_NEWSFEED_POST_WITH_IMAGE = gql `
+  mutation updateFeedPost($body: String!, $imgUrl: String!, $id: ID!) {
+    updateFeedPost(
+      data: {
+        body: $body
+        imgUrl: $imgUrl
+        }
+        where: {
+          id: $id
+          }) {
+            id
+            body
+            imgUrl
+            postedBy {
+              id
+              email
+            }
+            updatedAt
+          }
+        }
+`
+
+// Delete Newsfeed Post
+export const DELETE_NEWSFEED_POST = gql`
+  mutation deleteFeedPost($id: ID!) {
+    deleteFeedPost(where: { id: $id }) {
+      id
+    }
+  }
+`
+
+// Newsfeed Post Subscription
+export const NEWSFEED_POST_SUBSCRIPTION = gql `
+  subscription {
+  feedPost {
+    mutation
+    node {
+      id
+      body
+      imgUrl
+      likes {
+        id
+        likedBy {
+          id
+          email
+        }
+        post {
+          id
+        }
+      }
+      postedBy {
+        id
+        email
+      }
+      createdAt
+    }
+  }
+}
+`
