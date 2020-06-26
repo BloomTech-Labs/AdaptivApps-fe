7c1b1422c39f1c8f912324e94339378703c421de
Robert Campbell <campbell.rob@protonmail.com>
2020-06-26 08:28:22 -0700

    deleted some stuff

--- src/config/auth_config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  domain: process.env.REACT_APP_CLIENT_DOMAIN,
-  clientId: process.env.REACT_APP_CLIENT_ID,
-  roleUrl: 'http://adaptivapps.com/roles',
-  audience: process.env.REACT_APP_AUDIENCE,
-  responseType: 'token id_token',
-  scope: 'openid email',
-};
