139cbd2f07b54d925491201e9a29a6d85c655d8c
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-06-26 14:21:48 -0400

    adds tabIndex to waiverContainer in Step6.js

--- src/pages/UserSettings/AccountInfo/Step6.js
+++ src/pages/UserSettings/AccountInfo/Step6.js
@@ -153,7 +153,7 @@ export default function Step6({ updateDemo4 }) {
         <Typography className={classes.spacing}>
           Please read and acknowledge this Virtual Games Waiver of Liability
         </Typography>
-        <Box className={classes.waiverContainer}>
+        <Box className={classes.waiverContainer}tabIndex="0">
           <Typography className={classes.waiverText}>
             1. Waiver of Liability, Assumption of Risk, and Indemnity Agreement
             Waiver: In consideration of being permitted to participate in any
