5adffb12313565e4e52440acff673ab96f3e35a3
Amber Mohler <ambermohler@Ambers-iMac.local>
2020-06-29 19:57:16 -0400

    changes sponsor banner to figma design

--- src/pages/SponsorSpotlight/SponsorBanner.js
+++ src/pages/SponsorSpotlight/SponsorBanner.js
@@ -4,12 +4,13 @@ import { Container, Typography, Link } from "@material-ui/core";
 import { useNavigate } from "@reach/router";
 
 const ACS_Logo = require("../../assets/images/01-ACS_Logo.png");
-const Hartford_Logo = require("../../assets/images/HartfordBanner.png");
+const Hartford_Logo = require("../../assets/images/TheHartfordBlue.jpg");
 
 const useStyles = makeStyles(theme => ({
   banner: {
-    backgroundImage: " linear-gradient(#343c6d, #43496c 30%, #282a3c 80%)",
-    padding: "3%",
+    //backgroundImage: " linear-gradient(#343c6d, #43496c 30%, #282a3c 80%)",
+    backgroundColor: '#282c52',
+    padding: '1%',
     textAlign: "center",
     display: 'flex',
     justifyContent: 'space-evenly',
@@ -21,12 +22,15 @@ const useStyles = makeStyles(theme => ({
     fontWeight: "900",
     fontSize: "2.5rem",
     letterSpacing: "2px",
+    '&:hover': {
+      cursor: 'pointer'
+    },
   },
   acs: {
-    maxWidth: '20%'
+    maxWidth: '15%'
   },
   hartford: {
-    maxWidth: '40%'
+    maxWidth: '25%'
   }
 }));
 
@@ -39,9 +43,9 @@ export default function SponsorBanner() {
       <Container className={classes.banner}>
         <img className={classes.acs} src={ACS_Logo} alt='Angel City Virtual Games Logo' tabIndex='0' />
         <Typography className={classes.text}>
-          Thank You to Our Sponsors
+          Click to view all sponsors!
         </Typography>
-        {/* <img className={classes.hartford} src={Hartford_Logo} alt='Hartford Logo' tabIndex='0' /> */}
+        <img className={classes.hartford} src={Hartford_Logo} alt='Hartford Logo' tabIndex='0' />
       </Container>
     </Link>
   );
