315917de7c264c8376492df8a8e3adba6d5b21e0
Amber Mohler <ambermohler@Ambers-iMac.local>
2020-06-30 15:30:25 -0400

    fixes date styling on view details modal

--- src/pages/EventsCalendar/SimpleModal.js
+++ src/pages/EventsCalendar/SimpleModal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import moment from "moment";
 import Modal from "@material-ui/core/Modal";
 import { Container, Box, Button, Typography } from "@material-ui/core";
 import eventImg from "../../assets/images/acs_hartford.png";
@@ -140,7 +141,8 @@ export default function SimpleModal({ event, registerEvent }) {
       </Box>
       <Box className={classes.modalMiddle}>
         <Typography className={classes.date}>
-          {event.startDate} - {event.endDate}
+          {moment(event.startDate).format("MM/DD/YYYY")} -{" "}
+          {moment(event.endDate).format("MM/DD/YYYY")}
         </Typography>
         <Typography className={classes.title} id="simple-modal-title">
           {event.title}
