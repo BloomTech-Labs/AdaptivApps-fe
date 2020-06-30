eb81b928d028fbbb7a5e4057254b1fb5683f06d2
JonathanChen <jonny29009@yahoo.com>
2020-06-30 15:51:00 -0700

    finishes welcome page

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -22,6 +22,15 @@ const useStyles = makeStyles(theme => ({
     fontWeight: "400",
     borderColor: "#D3D3D3",
   },
+  body: {
+    marginLeft: "3rem",
+    marginBottom: "5rem",
+  },
+  h3: {
+    fontSize: "3rem",
+    color: "gray",
+    fontWeight: "normal",
+  }
 }));
 
 const Welcome = () => {
@@ -31,9 +40,27 @@ const Welcome = () => {
     <Box component="main" className={classes.root}>
       <Box className={classes.headingBox} borderBottom={2}>
         <Typography variant="h1" gutterBottom>
-          Welcome to Angel City Sports!
+          Welcome to the Angel City Sports platform!
         </Typography>
       </Box>
+      <div className={classes.body}>
+        <h3 className={classes.h3}>A welcome message from us to you</h3>
+        <iframe
+          width="700"
+          height="400"
+          src="https://www.youtube.com/embed/w77zPAtVTuI"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullscreen />
+        <h3 className={classes.h3}>A few tips for getting started on our platform</h3>
+        <iframe
+          width="700"
+          height="400"
+          src="https://www.youtube.com/embed/w77zPAtVTuI"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullscreen />
+      </div>
     </Box>
   )
 }
