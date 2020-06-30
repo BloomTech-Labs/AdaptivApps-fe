ed0df4d3b5ee2fe8bac1af6f06a9dd14ab329d40
JonathanChen <jonny29009@yahoo.com>
2020-06-30 16:03:46 -0700

    polishing faq page

--- src/pages/FAQ/index.js
+++ src/pages/FAQ/index.js
@@ -22,6 +22,18 @@ const useStyles = makeStyles(theme => ({
     fontWeight: "400",
     borderColor: "#D3D3D3",
   },
+  body: {
+    marginLeft: "3rem",
+    marginBottom: "5rem",
+  },
+  h4: {
+    fontSize: "2.5rem",
+    color: "gray",
+    fontWeight: "normal",
+  },
+  p: {
+    fontSize: "1.5rem"
+  }
 }));
 
 const FAQ = () => {
@@ -34,6 +46,84 @@ const FAQ = () => {
           Frequently Asked Questions
         </Typography>
       </Box>
+      <div className={classes.body}>
+        <div className={classes.group}>
+          <h3 className={classes.h4}>Here's a question that people often ask</h3>
+          <p className={classes.p}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae magna felis.
+            Integer eget auctor nisl. Nulla porttitor mauris vel cursus sodales. Nam ut velit non erat porta consequat.
+            Phasellus bibendum vitae nunc ac sodales. Aliquam erat volutpat. Proin sit amet elit egestas,
+            gravida nunc vitae, lobortis tellus. Mauris at augue ullamcorper leo tristique volutpat et in velit.
+          </p>
+          <p className={classes.p}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae magna felis.
+            Integer eget auctor nisl. Nulla porttitor mauris vel cursus sodales. Nam ut velit non erat porta consequat.
+            Phasellus bibendum vitae nunc ac sodales. Aliquam erat volutpat. Proin sit amet elit egestas,
+            gravida nunc vitae, lobortis tellus. Mauris at augue ullamcorper leo tristique volutpat et in velit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae magna felis.
+            Integer eget auctor nisl. Nulla porttitor mauris vel cursus sodales. Nam ut velit non erat porta consequat.
+            Phasellus bibendum vitae nunc ac sodales. Aliquam erat volutpat. Proin sit amet elit egestas,
+            gravida nunc vitae, lobortis tellus. Mauris at augue ullamcorper leo tristique volutpat et in velit.
+          </p>
+        </div>
+        <div className={classes.group}>
+          <h3 className={classes.h4}>Here's another question that people often ask</h3>
+          <p className={classes.p}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae magna felis.
+            Integer eget auctor nisl. Nulla porttitor mauris vel cursus sodales. Nam ut velit non erat porta consequat.
+            Phasellus bibendum vitae nunc ac sodales. Aliquam erat volutpat. Proin sit amet elit egestas,
+            gravida nunc vitae, lobortis tellus. Mauris at augue ullamcorper leo tristique volutpat et in velit.
+          </p>
+          <p className={classes.p}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae magna felis.
+            Integer eget auctor nisl. Nulla porttitor mauris vel cursus sodales. Nam ut velit non erat porta consequat.
+            Phasellus bibendum vitae nunc ac sodales. Aliquam erat volutpat. Proin sit amet elit egestas,
+            gravida nunc vitae, lobortis tellus. Mauris at augue ullamcorper leo tristique volutpat et in velit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae magna felis.
+            Integer eget auctor nisl. Nulla porttitor mauris vel cursus sodales. Nam ut velit non erat porta consequat.
+            Phasellus bibendum vitae nunc ac sodales. Aliquam erat volutpat. Proin sit amet elit egestas,
+            gravida nunc vitae, lobortis tellus. Mauris at augue ullamcorper leo tristique volutpat et in velit.
+          </p>
+        </div>
+        <div className={classes.group}>
+          <h3 className={classes.h4}>Here's yet another question that people often ask</h3>
+          <p className={classes.p}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae magna felis.
+            Integer eget auctor nisl. Nulla porttitor mauris vel cursus sodales. Nam ut velit non erat porta consequat.
+            Phasellus bibendum vitae nunc ac sodales. Aliquam erat volutpat. Proin sit amet elit egestas,
+            gravida nunc vitae, lobortis tellus. Mauris at augue ullamcorper leo tristique volutpat et in velit.
+          </p>
+          <p className={classes.p}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae magna felis.
+            Integer eget auctor nisl. Nulla porttitor mauris vel cursus sodales. Nam ut velit non erat porta consequat.
+            Phasellus bibendum vitae nunc ac sodales. Aliquam erat volutpat. Proin sit amet elit egestas,
+            gravida nunc vitae, lobortis tellus. Mauris at augue ullamcorper leo tristique volutpat et in velit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae magna felis.
+            Integer eget auctor nisl. Nulla porttitor mauris vel cursus sodales. Nam ut velit non erat porta consequat.
+            Phasellus bibendum vitae nunc ac sodales. Aliquam erat volutpat. Proin sit amet elit egestas,
+            gravida nunc vitae, lobortis tellus. Mauris at augue ullamcorper leo tristique volutpat et in velit.
+          </p>
+        </div>
+        <div className={classes.group}>
+          <h3 className={classes.h4}>Aaaaaaand another question that people often ask</h3>
+          <p className={classes.p}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae magna felis.
+            Integer eget auctor nisl. Nulla porttitor mauris vel cursus sodales. Nam ut velit non erat porta consequat.
+            Phasellus bibendum vitae nunc ac sodales. Aliquam erat volutpat. Proin sit amet elit egestas,
+            gravida nunc vitae, lobortis tellus. Mauris at augue ullamcorper leo tristique volutpat et in velit.
+          </p>
+          <p className={classes.p}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae magna felis.
+            Integer eget auctor nisl. Nulla porttitor mauris vel cursus sodales. Nam ut velit non erat porta consequat.
+            Phasellus bibendum vitae nunc ac sodales. Aliquam erat volutpat. Proin sit amet elit egestas,
+            gravida nunc vitae, lobortis tellus. Mauris at augue ullamcorper leo tristique volutpat et in velit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae magna felis.
+            Integer eget auctor nisl. Nulla porttitor mauris vel cursus sodales. Nam ut velit non erat porta consequat.
+            Phasellus bibendum vitae nunc ac sodales. Aliquam erat volutpat. Proin sit amet elit egestas,
+            gravida nunc vitae, lobortis tellus. Mauris at augue ullamcorper leo tristique volutpat et in velit.
+          </p>
+        </div>
+      </div>
     </Box>
   )
 }
