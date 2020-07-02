4fd7853185b3a8e5fb79386c30b5248cdcdedd46
Robert Campbell <campbell.rob@protonmail.com>
2020-07-02 16:39:39 -0700

    updated bio font size

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -166,7 +166,7 @@ const useStyles = makeStyles(theme => ({
     maxWidth: "80rem",
     "& p": {
       marginLeft: "1rem",
-      fontSize: "1.2rem",
+      fontSize: "1.6rem",
     },
     "& h1": {
       fontSize: "1.4rem",
