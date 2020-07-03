a598082a944a982b80c9878e543e8c2736bda936
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-07-03 15:20:10 -0400

    makes select menus accessible on AccountTypeForm.js

--- src/pages/UserSettings/AccountInfo/AccountTypeForm.js
+++ src/pages/UserSettings/AccountInfo/AccountTypeForm.js
@@ -153,25 +153,25 @@ export default function AccountTypeForm({ updateProfile }) {
       {/* <ProgressBar activeStep={0} userEmail={userEmail} /> */}
       <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
         <Box className={classes.selectContainer}>
-          <InputLabel className={classes.label} required for="accountType">
+          <InputLabel className={classes.label} required id="accountType">
             Are you registering as an individual or an organization?
           </InputLabel>
           {errors.type && (
-            <Typography className={classes.error}>
+            <Typography className={classes.error} role="alert">
               Please make a selection
             </Typography>
           )}
-          {/* <Box className={classes.box}> */}
+          <Box className={classes.box}>
             <Controller
               as={
                 <Select className={classes.typeSelect} aria-label="account type">
                   {/* <MenuItem value="">
                     <em className={classes.em}>Please choose one</em>
                   </MenuItem> */}
-                  <MenuItem value="Individual">
+                  <MenuItem value="Individual" aria-labelledby="accountType">
                     I'm registering as an individual
                   </MenuItem>
-                  <MenuItem value="Organization">
+                  <MenuItem value="Organization" aria-labelledby="accountType">
                     I'm registering as an organization
                   </MenuItem>
                 </Select>
@@ -182,41 +182,44 @@ export default function AccountTypeForm({ updateProfile }) {
               control={control}
               defaultValue=""
               rules={{ required: true }}
+              aria-invalid={errors.type ? "true" : "false"}
             />
-          {/* </Box> */}
-          <InputLabel required htmlFor="role identity">
+          </Box>
+          <InputLabel required id="roleIdentity">
             Which role do you best identify with?
           </InputLabel>
           {errors.roleIdentity && (
-            <Typography className={classes.error}>
+            <Typography className={classes.error} role="alert">
               Please make a selection
             </Typography>
           )}
-          {/* <Box className={classes.box}> */}
+          <Box className={classes.box}>
             <Controller
               as={
                 <Select className={classes.typeSelect} ariaLabel="role types">
                   {/* <MenuItem value="">
                     <em className={classes.em}>Please choose one</em>
                   </MenuItem> */}
-                  <MenuItem value="Athlete">Adaptive Athlete</MenuItem>
-                  <MenuItem value="Ally/Volunteer">
+                  <MenuItem value="Athlete" aria-labelledby="roleIdentity">Adaptive Athlete</MenuItem>
+                  <MenuItem value="Ally/Volunteer" aria-labelledby="roleIdentity">
                     Ally/Volunteer - I want to participate and will volunteer to
                     help promote.
                   </MenuItem>
-                  <MenuItem value="Donor/Supporter">
+                  <MenuItem value="Donor/Supporter" aria-labelledby="roleIdentity">
                     Donor/Supporter - I want to participate and will donate or
                     help fundraise.
                   </MenuItem>
                 </Select>
               }
+              id="roleIdentity"
               name="roleIdentity"
               variant="outlined"
               control={control}
               defaultValue=""
               rules={{ required: true }}
+              aria-invalid={errors.roleIdentity ? "true" : "false"}
             />
-          {/* </Box> */}
+          </Box>
           <Typography className={classes.error}>* required field</Typography>
         </Box>
         <Box className={classes.btnWrapper}>
